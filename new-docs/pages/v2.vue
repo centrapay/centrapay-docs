982bfeadc8e0e5bdf1e54dd950937da076ac12c3
Raymond Yang <raymond.yang@centrapay.com>
2022-08-22 14:35:37 +1200

    Nuxt3 Hello World
    Created a Hello World Nuxt3 project to be deployed alongside the existing docs at /v2

---
+++ new-docs/pages/v2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <button @click="routeToDocs">Old Docs</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Hello World',
+    }
+  },
+
+  methods: {
+    routeToDocs() {
+      window.location = 'https://docs.centrapay.com';
+    }
+  },
+}
+</script>
