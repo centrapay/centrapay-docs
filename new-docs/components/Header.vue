bd8ec1becf20fb2430e966884d65cecd06974bed
Raymond Yang <raymond.yang@centrapay.com>
2022-08-25 10:27:00 +1200

    Make use of default Nuxt 3 exports

--- new-docs/components/Header.vue
+++ new-docs/components/Header.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="w-full p-4 shadow-md">
     <div class="desktop-gutters flex items-center justify-center space-x-2">
-      <centrapay-logo class="w-12 h-12 fill-brand-accent" />
+      <lazy-icons-centrapay-logo class="w-12 h-12 fill-brand-accent" />
       <span class="hidden md:block type-headline-3">Centrapay Docs</span>
     </div>
   </div>
 </template>
-
-<script setup>
-import { defineAsyncComponent } from 'vue';
-const CentrapayLogo = defineAsyncComponent(() => import('./icons/CentrapayLogo.vue'));
-</script>
