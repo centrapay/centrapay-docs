75c5d18a74e052d4774e71957e55ab4ada64ec1c
Raymond Yang <raymond.yang@centrapay.com>
2022-08-25 12:36:05 +1200

    Eagerly load the header icon

--- new-docs/components/Header.vue
+++ new-docs/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="w-full p-4 shadow-md">
     <div class="desktop-gutters flex items-center justify-center space-x-2">
-      <lazy-icons-centrapay-logo class="w-12 h-12 fill-brand-accent" />
+      <icons-centrapay-logo class="w-12 h-12 fill-brand-accent" />
       <span class="hidden md:block type-headline-3">Centrapay Docs</span>
     </div>
   </header>
