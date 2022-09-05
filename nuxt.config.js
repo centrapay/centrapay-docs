e03a08d3ae35b2254eaef096a117c491a53e2513
Raymond Yang <raymond.yang@centrapay.com>
2022-09-05 16:06:46 +1200

    Add Centrapay favicon and Centrapay Docs title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,11 @@ import { defineNuxtConfig } from 'nuxt';
 export default defineNuxtConfig({
   css: ['~/assets/css/tailwind.css'],
   app: {
-    buildAssetsDir: '/nuxt/'
+    buildAssetsDir: '/nuxt/',
+    head: {
+      title: 'Centrapay Docs',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
   },
   build: {
     postcss: {
