2ebeaf500c6517bd29a0cda473a1c360a39f85e1
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-01-13 10:14:53 +1300

    🐛 FIX: Add divider above h2 headings
    This styling was removed in Pull Request #561.
    
    Test plan: See divider above h2 headings as pictured.

--- components/Prose.vue
+++ components/Prose.vue
@@ -3,8 +3,8 @@
     class="
       prose prose-slate max-w-none
       prose-headings:font-display prose-headings:font-normal
+      prose-h2:scroll-mt-16 prose-h2:mt-8 prose-h2:pt-8 prose-h2:border-t
       prose-h3:scroll-mt-20
-      prose-h2:scroll-mt-24
       prose-lead:text-slate-500
       prose-a:no-underline
       prose-pre:bg-transparent prose-pre:my-0
