2dd674eb7c10b3e1cf92f5c76452bae9940a9e9d
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-01-17 13:17:24 +1300

    Style nav text

--- components/AppShell.vue
+++ components/AppShell.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-h-full">
     <TransitionRoot
       as="template"
       :show="sidebarOpen"
@@ -53,35 +53,20 @@
                   </div>
                 </div>
               </TransitionChild>
-              <div class="flex flex-shrink-0 items-center px-4">
-                <centrapay-logo class="text-brand-accent" />
-              </div>
-              <div class="mt-6 h-0 flex-1 overflow-y-auto">
+              <div class="h-0 flex-1 overflow-y-auto">
                 <nav class="space-y-1 px-2">
-                  <div class="pt-1 pb-4">
+                  <div class="pt-1 pb-4 space-y-1">
                     <NuxtLink
                       v-for="item in topBarNavigation"
                       :key="item.name"
                       :href="item.href"
                       target="_blank"
-                      class="text-content-primary group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                      class="text-content-secondary group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md focus:outline-none focus:ring-2 ring-focus-ring"
+                      :class="item.current ? 'bg-gray-100': ''"
                     >
                       {{ item.name }}
                     </NuxtLink>
                   </div>
-                  <NuxtLink
-                    v-for="item in navigation"
-                    :key="item.title"
-                    :href="item._path"
-                    class="text-content-primary group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                  >
-                    <!-- <component
-                      :is="item.icon"
-                      class="text-content-tertiary mr-4 flex-shrink-0 icon-sm"
-                      aria-hidden="true"
-                    /> -->
-                    <span class="text-content-secondary text-base leading-6 font-medium">{{ item.title }}</span>
-                  </NuxtLink>
                 </nav>
               </div>
             </DialogPanel>
@@ -100,11 +85,19 @@
       <!-- FIXME: This should be a button -->
       <div
         type="button"
-        class="flex items-center px-4 focus:outline-none focus:ring-2 focus:ring-inset ring-focus-ring md:hidden"
+        class="flex w-full items-center justify-between px-6 focus:outline-none focus:ring-2 focus:ring-inset ring-focus-ring md:hidden"
         @click="sidebarOpen = true"
       >
-        <span class="sr-only">Open sidebar</span>
-        <navigation-menu class="text-content-tertiary" />
+        <div>
+          <span class="sr-only">Open sidebar</span>
+          <navigation-menu class="text-content-tertiary" />
+        </div>
+        <NuxtLink
+          href="/"
+          class="flex items-center justify-center rounded-full h-7 w-7 bg-static-black"
+        >
+          <centrapay-logo class="text-content-on-color icon-md" />
+        </NuxtLink>
       </div>
       <div class="hidden md:flex md:flex-1 md:justify-between md:py-3">
         <div class="ml-4 flex items-center md:ml-6">
@@ -120,7 +113,7 @@
               :key="item.name"
               :href="item.href"
               target="_blank"
-              class="text-gray-600 text-sm leading-5 font-medium px-3 py-2 rounded-lg hover:bg-gray-200 hover:text-content-primary"
+              class="text-gray-600 text-sm leading-5 font-medium px-3 py-2 rounded-lg hover:bg-gray-200 hover:text-content-primary focus:ring-2 ring-focus-ring"
               :class="item.current ? 'bg-gray-100': ''"
             >
               {{ item.name }}
@@ -132,7 +125,6 @@
 
     <!-- Static sidebar for desktop -->
     <div class="hidden md:fixed md:h-full md:inset-y-20 md:flex md:w-64 md:flex-col">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white">
         <div class="mt-5 flex flex-grow flex-col">
           <nav
@@ -148,50 +140,56 @@
                 class="space-y-1"
               >
                 <DisclosureButton
-                  class="group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md focus:outline-none focus:ring-2 ring-focus-ring"
-                  :class="true ? 'bg-gray-100 text-gray-900' : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900'"
-                  @click="setCurrentPath(item._path)"
+                  class="group w-full flex items-center pl-2 pr-1 py-2 text-left text-content-primary text-base leading-6 font-medium rounded-md hover:bg-gray-200"
+                  :class="currentPath.startsWith(item._path) ? 'bg-gray-100' : ''"
+                  @click="setActiveDisclosure(item._path)"
                 >
-                  <!-- <component
-                    :is="item.icon"
-                    class="mr-3 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                    aria-hidden="true"
-                  /> -->
                   <span class="flex-1">{{ item.title }}</span>
                 </DisclosureButton>
-                <div v-show="currentPath.startsWith(item._path)">
-                  <DisclosurePanel
-                    static
-                    class="space-y-1"
-                  >
-                    <div
-                      v-for="firstChild in item.children"
-                      :key="firstChild.title"
+                <transition
+                  enter-active-class="transition duration-100 ease-out"
+                  enter-from-class="transform scale-95 opacity-0"
+                  enter-to-class="transform scale-100 opacity-100"
+                  leave-active-class="transition duration-75 ease-out"
+                  leave-from-class="transform scale-100 opacity-100"
+                  leave-to-class="transform scale-95 opacity-0"
+                >
+                  <div v-show="showPanel(item._path)">
+                    <DisclosurePanel
+                      static
+                      class="space-y-1"
                     >
-                      <DisclosureButton
-                        as="a"
-                        :href="firstChild._path"
-                        class="group flex w-full items-center rounded-md py-2 pl-6 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                      <div
+                        v-for="firstChild in item.children"
+                        :key="firstChild.title"
                       >
-                        {{ firstChild.title }}
-                      </DisclosureButton>
-                      <div v-if="firstChild.children">
-                        <div
-                          v-for="secondChild in firstChild.children"
-                          :key="secondChild.title"
+                        <DisclosureButton
+                          as="a"
+                          :href="firstChild._path"
+                          class="group flex w-full items-center rounded-md py-2 pl-6 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                          :class="currentPath == firstChild._path ? 'bg-light-surface-tertiary text-content-primary' : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900'"
                         >
-                          <DisclosureButton
-                            as="a"
-                            :href="secondChild._path"
-                            class="group flex w-full items-center rounded-md py-2 pl-8 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                          {{ firstChild.title }}
+                        </DisclosureButton>
+                        <div v-if="firstChild.children">
+                          <div
+                            v-for="secondChild in firstChild.children"
+                            :key="secondChild.title"
                           >
-                            {{ secondChild.title }}
-                          </DisclosureButton>
+                            <DisclosureButton
+                              as="a"
+                              :href="secondChild._path"
+                              class="group flex w-full items-center rounded-md py-2 pl-8 pr-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                              :class="currentPath == secondChild._path ? 'bg-light-surface-tertiary text-content-primary' : 'bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900'"
+                            >
+                              {{ secondChild.title }}
+                            </DisclosureButton>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </DisclosurePanel>
-                </div>
+                    </DisclosurePanel>
+                  </div>
+                </transition>
               </Disclosure>
             </div>
           </nav>
@@ -231,9 +229,18 @@ const topBarNavigation = [
 
 const sidebarOpen = ref(false);
 const { page, navigation } = useContent();
-const currentPath = ref(page.value._path);
+const currentPath = page.value._path;
+const activeDisclosure = ref(currentPath);
 
-function setCurrentPath(val) {
-  currentPath.value = val;
+function showPanel(path) {
+  return currentPath.includes(path) || activeDisclosure.value.includes(path);
+}
+
+function setActiveDisclosure(val) {
+  if (activeDisclosure.value == val) {
+    activeDisclosure.value = '';
+  } else {
+    activeDisclosure.value = val;
+  }
 }
 </script>
