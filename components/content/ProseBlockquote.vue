9bed3d442919ff79bde54fca059fbd101387baf7
Andy Clifford <andrew.clifford@centrapay.com>
2023-01-18 16:47:37 +1300

    Override ProseBlockquote component
    Blockquote  component which is to be used in our markdown guides. We don't currently have a use case but when we migrate the rest of the content it will be.

---
+++ components/content/ProseBlockquote.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="not-prose flex p-5 space-x-5 bg-surface-secondary rounded-lg">
+    <information-circle class="icon-md text-static-black" />
+    <blockquote class="text-content-tertiary m-0 text-sm leading-5 font-normal">
+      <slot />
+    </blockquote>
+  </div>
+</template>
