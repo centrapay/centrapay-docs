854764885947336d891e3e0e6def6728505bde90
Raymond Yang <raymond.yang@centrapay.com>
2023-01-20 15:11:59 +0800

    Create 404 page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<template>
+  <main
+    class="flex flex-col justify-center items-center min-h-screen w-full"
+  >
+    <div class="space-y-1 flex flex-col items-center">
+      <span class="type-body-2 text-brand-accent">404</span>
+      <h1 class="type-headline-3">
+        Page not found
+      </h1>
+      <span class="type-body-2 text-center">Please check the URL in the address bar and try again.</span>
+    </div>
+    <div class="flex mt-8 space-x-2 h-12 w-35">
+      <NuxtLink
+        class="btn-primary rounded-md h-full flex items-center px-4"
+        to="/"
+      >
+        <span class="type-caption-2 text-content-inverse-primary">Go Back Home</span>
+      </NuxtLink>
+      <a
+        class="btn-tertiary rounded-md border h-full flex items-center px-4"
+        href="mailto:integrations@centrapay.com"
+      >
+        <span class="type-caption-2 text-content-tertiary">Contact Support</span>
+      </a>
+    </div>
+  </main>
+</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,3 +5,12 @@
     </Layout>
   </main>
 </template>
+
+<script setup>
+const { page } = useContent();
+// FIXME remove second expression once farmlands partner page has been moved to markdown
+const pageExists = page.value._file || page.value._path === '/connections/farmlands' || page.value._path === '/';
+if (!pageExists) {
+  throw createError({ statusCode: 404, statusMessage: 'Not Found' });
+}
+</script>
