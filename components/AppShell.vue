44b3205202648a02a3810f37c4381343b3e9530e
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-01-26 11:48:30 +1300

    🐛 Fix: Open disclosure of active navigation item when link in body is clicked
    Previous behaviour: If you were on the Farmlands POS Integration guide and clicked a link in the body to a page under 'References', the 'References' section would show as active but the disclosure would not open.
    Change to open the disclosure of the active navigation section when the route changes.
    
    Test plan: Expect disclosure of active navigation section to open when route changes.

--- components/AppShell.vue
+++ components/AppShell.vue
@@ -71,7 +71,7 @@
     </div>
 
     <!-- Static sidebar for desktop -->
-    <div class="hidden border-t md:fixed md:h-full md:inset-y-16 md:flex md:w-64 xl:w-80 md:flex-col">
+    <div class="hidden md:fixed md:h-full md:inset-y-16 md:flex md:w-64 xl:w-80 md:flex-col">
       <div class="flex flex-grow flex-col overflow-y-auto pt-5 border-r border-gray-200 bg-white">
         <div class="flex flex-grow flex-col">
           <nav
