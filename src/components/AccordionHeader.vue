bd6f6ed2eb8a433ac7cdb4f39e2b2893f872ff19
Isaac Murtagh <isaac.murtagh@centrapay.com>
2023-02-27 12:45:31 +1300

    Use astro component for accordion item

---
+++ src/components/AccordionHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col overflow-hidden shadow-xl rounded-lg">
+    <!-- <p>
+      <slot name="title"/>
+    </p>
+    <p>
+      <slot name="description"/>
+    </p> -->
+    <slot name="title" />
+    <button @click="() => console.log('hello')">click me</button>
+  </div>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue'
+const emits = defineEmits(['click'])
+</script>
--- src/components/AccordionItem.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="flex flex-col overflow-hidden shadow-xl rounded-lg">
-    <p>
-      **hi**
-    </p>
-  </div>
-</template>
