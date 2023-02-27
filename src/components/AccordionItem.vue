7f563cdbe648fa03133275499a1c4a0c13058e58
Laurence McKnight <laurence.mcknight@centrapay.com>
2023-02-28 11:02:30 +1300

    Finalise accordion item component

---
+++ src/components/AccordionItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="flex flex-row space-x-4">
+      <slot name="title" />
+      <button
+        class="focus:ring-0"
+        @click="toggleDescription()"
+      >
+        <ChevronDown :class="[showDescription && 'rotate-180']" />
+      </button>
+    </div>
+    <slot
+      v-if="showDescription"
+      name="description"
+    />
+  </div>
+</template>
+
+<script>
+import ChevronDown from './icons/ChevronDown.vue';
+
+export default {
+  components: {
+    ChevronDown,
+  },
+
+  data() {
+    return {
+      showDescription: false,
+    };
+  },
+
+  methods: {
+    toggleDescription() {
+      this.showDescription = !this.showDescription;
+    },
+  },
+};
+</script>
