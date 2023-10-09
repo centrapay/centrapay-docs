51f1bb9a2d7dfe054d6bc5019d63e3c3f56f0f48
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-10-09 16:05:08 +1100

    ⏰ [10% time]: ♻️ Refactor plain disclosure component
    * Use disclosure open slot to manage open state. This means we do not need to listen to clicks or key bindings.
    * Add disclosure transition for consistency.
    * Make carat move consistently.
    
    Test plan: Expect changes listed to appear in production.

--- src/components/Disclosure.vue
+++ src/components/Disclosure.vue
@@ -1,15 +1,16 @@
 <template>
-  <Disclosure>
+  <Disclosure v-slot="{ open }">
     <DisclosureButton
       class="flex w-full flex-row text-left justify-between px-0 my-4 focus:ring-1 ring-offset-8 rounded-md type-subtitle-2"
-      @click="open = !open"
     >
       {{ title }}
-      <Carat :class="[open ? '-rotate-90' : 'rotate-90', 'ml-2 h-6 w-6 flex-none']" />
+      <Carat :class="[open && 'rotate-90', 'ml-2 h-6 w-6 flex-none']" />
     </DisclosureButton>
-    <DisclosurePanel class="mr-6 mb-2">
-      <slot />
-    </DisclosurePanel>
+    <DisclosureTransition>
+      <DisclosurePanel class="mr-6 mb-2">
+        <slot />
+      </DisclosurePanel>
+    </DisclosureTransition>
   </Disclosure>
 </template>
 
@@ -21,10 +22,9 @@ import {
   DisclosureButton,
   DisclosurePanel,
 } from '@headlessui/vue';
+import DisclosureTransition from './DisclosureTransition.vue';
 
 const props = defineProps({
   title: { type: String, required: true },
 });
-
-let open = ref(false);
 </script>
