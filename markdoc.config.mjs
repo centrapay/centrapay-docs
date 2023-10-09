06fb283ea9c9ebb38d45566df3fdd3be79320e0d
Andy Clifford <andrew.clifford@centrapay.com>
2023-10-09 15:08:17 +1300

    Endpoint component with markdoc
    This change allows us to document api endpoints using markdoc using the same layout as seen in the [Protocol Tailwind UI Example](https://protocol.tailwindui.com/contacts#list-all-contacts).
    
    There are issues with the styling such as the h2 header but we are just focussed on getting the layout correct then will followup with styling later once we have aligned with the designers.

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,13 @@
 import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
+import shiki from '@astrojs/markdoc/shiki';
 
 export default defineMarkdocConfig({
+  extends: [
+    shiki({
+      theme: 'github-light',
+      langs: [],
+    }),
+  ],
   tags: {
     properties: {
       render: component('./src/components/Properties.astro'),
@@ -11,6 +18,25 @@ export default defineMarkdocConfig({
         name: { type: String, required: true },
         type: { type: String, required: true },
       },
-    }
+    },
+    endpoint: {
+      render: component('./src/components/Endpoint.astro'),
+      attributes: {
+        method: { type: String, required: true },
+        path: { type: String, required: true },
+      },
+    },
+    row: {
+      render: component('./src/components/Row.astro'),
+    },
+    column: {
+      render: component('./src/components/Column.astro'),
+      attributes: {
+        sticky: { type: Boolean },
+      },
+    },
+    code: {
+      render: component('./src/components/Code.astro'),
+    },
   }
 });
