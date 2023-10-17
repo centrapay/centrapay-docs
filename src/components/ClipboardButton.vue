c0a8cd122351828179aa7cc43a6c0550ca20abdd
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-10-17 14:24:53 +1100

    Style clipboard button

--- src/components/ClipboardButton.vue
+++ src/components/ClipboardButton.vue
@@ -6,48 +6,45 @@
       class="groups relative inline-block transition hover:scale-110 active:scale-100 active:transition-none focus:ring-0"
       @mouseover="buttonHover = true"
       @mouseleave="mouseleave"
-      @click="tooltipText = 'Copied'"
+      @click="clickHandler"
     >
-      <div>
-        <span
-          id="copy-button-label"
-          hidden
+      <span
+        id="copy-button-label"
+        hidden
+      >
+        Copy to clipboard
+      </span>
+      <Clipboard
+        aria-hidden="true"
+        :class="buttonActive ?
+          'text-interactive-quaternary' :
+          'text-content-inverse-tertiary hover:text-interactive-tertiary-hover'
+        "
+      />
+      <div
+        v-show="buttonHover"
+        id="clipboard-tooltip"
+        :class="buttonActive ?
+          'bg-surface-positive text-content-inverse-primary' :
+          'bg-interactive-tertiary-hover text-content-secondary'
+        "
+        class="type-caption-2 w-16 bg-surface-primary rounded-lg py-1 px-2 absolute z-10 group-hover:opacity-100 bottom-full -left-[15px] pointer-events-none"
+      >
+        {{ tooltipText }}
+        <svg
+          class="absolute h-2 w-full left-0 top-[98%] z-20"
+          :class="buttonActive ? 'text-surface-positive' : 'text-interactive-tertiary-hover'"
+          x="0px"
+          y="0px"
+          viewBox="0 0 255 255"
+          xml:space="preserve"
         >
-          Copy to clipboard
-        </span>
-        <Clipboard
-          aria-hidden="true"
-          class="text-content-inverse-tertiary hover:text-interactive-tertiary-hover active:text-interactive-quaternary"
-        />
+          <polygon
+            class="fill-current"
+            points="0,0 127.5,127.5 255,0"
+          />
+        </svg>
       </div>
-      <Transition>
-        <div
-          v-show="buttonHover"
-          class="type-caption-2 w-32 drop-shadow bg-surface-primary rounded-lg py-1 px-2 absolute z-10 group-hover:opacity-100 bottom-full -left-[47px] pointer-events-none"
-        >
-          {{ tooltipText }}
-          <svg
-            class="absolute text-surface-primary h-2 w-full left-0 top-full"
-            x="0px"
-            y="0px"
-            viewBox="0 0 255 255"
-            xml:space="preserve"
-          >
-            <polygon
-              class="fill-current"
-              points="0,0 127.5,127.5 255,0"
-            />
-          </svg>
-        </div>
-        <!-- <span
-          v-show="buttonHover"
-          id="tooltip-hover"
-          role="tooltip"
-          class="bg-pink-500 absolute bottom-full -left-10 w-28"
-        >
-          {{ tooltipText }}
-        </span> -->
-      </Transition>
     </button>
   </div>
 </template>
@@ -56,12 +53,19 @@
 import { ref } from 'vue';
 import Clipboard from './icons/Clipboard.vue';
 
-const initialTooltipText = 'Click to copy';
+const initialTooltipText = 'Copy';
 const tooltipText = ref(initialTooltipText);
+const buttonActive = ref(false);
 const buttonHover = ref(false);
+
+function clickHandler() {
+  buttonActive.value = true;
+  tooltipText.value = 'Copied';
+}
 
 function mouseleave() {
   buttonHover.value = false;
+  buttonActive.value = false;
   window.setTimeout(() => {
     tooltipText.value = initialTooltipText;
   }, 75);
