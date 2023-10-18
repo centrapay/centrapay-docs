dad3cfba219b0b89e9720abd3e613b16478001ab
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-10-18 13:37:34 +1100

    💅 Fix styling

--- src/components/ProseBlockquote.vue
+++ src/components/ProseBlockquote.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="not-prose flex p-5 space-x-5 bg-surface-secondary rounded-lg">
+  <div class="not-prose my-5 flex p-5 space-x-5 bg-surface-secondary rounded-lg">
     <InformationCircle class="icon-md text-content-secondary w-6 h-6" />
     <blockquote class="text-content-tertiary m-0 text-sm leading-5 font-normal flex-1">
       <slot />
