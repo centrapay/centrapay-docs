b253f9cdf0f2e2029ac3b2e9df4337115f6dfc68
Alex Binns <alex.binns@centrapay.com>
2023-11-22 13:05:02 +1300

    Index page title and description

--- src/plugins/flexsearch.js
+++ src/plugins/flexsearch.js
@@ -4,7 +4,6 @@ import { promisify } from 'util';
 import { rehype } from 'rehype';
 import { visit } from 'unist-util-visit';
 import { toString } from 'mdast-util-to-string';
-import { findAfter } from 'unist-util-find-after';
 
 function slugify(str) {
   return str.toLowerCase().replace(/\s/g, '-');
@@ -18,8 +17,16 @@ function isH2(node) {
   return node.tagName === 'h2';
 }
 
-function isParagraph(node) {
-  return node.tagName === 'p';
+function isHead(node) {
+  return node.tagName == 'head';
+}
+
+function isTitle(node) {
+  return node.tagName == 'title';
+}
+
+function isDescription(node) {
+  return node.tagName == 'meta' && node.properties.name === 'description';
 }
 
 function formatSearch({ node, root, currentH2Heading }) {
@@ -33,18 +40,30 @@ function formatSearch({ node, root, currentH2Heading }) {
       pageContext: currentH2Heading,
     };
   default:
-    let description;
-    try {
-      description = toString(findAfter(root, node, isParagraph));
-    } catch (err) {
-      description = undefined;
-    }
     return {
       title: nodeString,
-      description,
+      description: 'hello',
       anchor: nodeString,
     };
   }
+}
+
+function processHeader({ root }) {
+  let title;
+  let description;
+  visit(root, 'element', (node) => {
+    if (isTitle(node)) {
+      title = toString(node).replace(/\s-\sCentrapay\sDocs/, '');
+    };
+    if (isDescription(node)){
+      description = node.properties.content;
+    }
+  });
+  return {
+    type: 'title',
+    title,
+    description,
+  };
 }
 
 function extractSections(root) {
@@ -57,6 +76,11 @@ function extractSections(root) {
     }
     if (isH3(node)) {
       sections.push(formatSearch({ node, root, currentH2Heading }));
+    }
+    if (isHead(node)) {
+      sections.push(processHeader({
+        root: node,
+      }));
     }
   });
 
@@ -74,19 +98,28 @@ async function createFlexsearchIndexData() {
   const indexData = {};
   const processor = rehype().use(rehypeExtractSections);
 
-  for (const filepath of await promisify(glob)('dist/**/index.html')) {
+  for (const filepath of await promisify(glob)('dist/api/accounts/index.html')) {
     const href = filepath.replace(/(dist)|index\.html/g, '');
     const content = await fs.readFile(filepath, 'utf-8');
 
     const vfile = await processor.process(content);
 
     // Add all other headings and their descriptions to the index
-    vfile.data.sections.forEach(({ title, description, anchor, pageContext }) => {
-      indexData[id++] = {
+    vfile.data.sections.forEach(({ page, description, anchor, pageContext, type, title }) => {
+      if(type == 'title') {
+        indexData[id++] = {
+          // path: pageContext ? path.concat(pageContext) : path,
+          title,
+          description,
+          href,
+        };
+      } else {
+        indexData[id++] = {
         // path: pageContext ? path.concat(pageContext) : path,
-        title,
-        description,
-        href: `${href}#${slugify(anchor)}`,
+          title,
+          description,
+          href: `${href}#${slugify(anchor)}`,
+        };
       };
     });
   }
