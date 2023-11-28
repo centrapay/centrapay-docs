0f08956e1ebcffa886c371b4e1d8a36808e2c782
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-11-29 08:12:21 +1100

    ✂️ Account Memberships: Generate code snippets (#898)
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests on Account Memberships page.

---
+++ src/content/api/_endpoints/account-memberships-delete.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'DELETE',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/members/45dwes-rf4h55-tyf45s-6st4gd',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
