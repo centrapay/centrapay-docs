6950104e0e5467b9011a41847523ce547b8f7525
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-11-29 08:14:55 +1100

    ✂️ Businesses: Generate code snippets (#899)
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests on Businesses page.

---
+++ src/content/api/_endpoints/businesses-update.js
@@ -0,0 +1,17 @@
+export default {
+  method: 'PUT',
+  path: '/api/businesses/DKTs3U38hdhfEqwF1JKoT2',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      taxNumber: {
+        value: '123-456-789',
+        type: 'nz-gst'
+      },
+      farmlandsBusinessNumber: '12345678'
+    },
+  },
+};
---
+++ src/content/api/_endpoints/nzbn-get.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'GET',
+  path: '/api/nzbn/9429046246448',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
