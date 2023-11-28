8a8e450cf87a751519b98cacded88a3e8bee6015
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-11-29 08:00:38 +1100

    ✂️ Accounts: Generate code snippets (#897)
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests on Accounts page.

---
+++ src/content/api/_endpoints/accounts-subscriptions-update.js
@@ -0,0 +1,15 @@
+export default {
+  method: 'PUT',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/subscriptions',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      subscriptions: [
+        'quartz'
+      ]
+    },
+  },
+};
