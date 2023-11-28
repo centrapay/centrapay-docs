b3a5588f18aa53efcea0c001cfd0fb1e0ca3aeab
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-29 08:41:51 +1100

    ✂️ Tokens, wallets, external assets: generate code snippets
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests.

---
+++ src/content/api/_endpoints/wallets-list-transactions.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'GET',
+  path: '/api/wallets/WRhAxxWpTKb5U7pXyxQjjY/transactions',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
---
+++ src/content/api/_endpoints/wallets-list.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'GET',
+  path: '/api/wallets',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
