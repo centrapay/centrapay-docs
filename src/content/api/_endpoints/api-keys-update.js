2a21f86aad37542f5ae21ddeb81ea4ed5aaa4706
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-28 15:38:36 +1100

    ✂️ API Keys: Generate code snippets
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests on API Keys page.

---
+++ src/content/api/_endpoints/api-keys-update.js
@@ -0,0 +1,13 @@
+export default {
+  method: 'PUT',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/api-keys/MyAPIkey',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      enabled: false
+    },
+  },
+};
