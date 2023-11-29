0e665e27d2f12fd10cfdc5eb20341d0c5d054894
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-11-29 11:35:48 +1100

    ✂️ Bank Account Connection Intents: generate code snippets (#907)
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests.

---
+++ src/content/api/_endpoints/bank-account-connection-intents-authorize.js
@@ -0,0 +1,13 @@
+export default {
+  method: 'POST',
+  path: '/api/bank-account-connection-intents/3KVjuKW2CZCJeJVqPxwkX7/authorize',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      code: 'hKpKupTM391pE10xfQiorMxXarRKAHRhTfH_x'
+    },
+  },
+};
