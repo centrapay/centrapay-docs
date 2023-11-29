a67ab9b077fc9664695c4bd5adb8ecd4ca56d072
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-11-29 14:57:14 +1100

    ✂️ Merchants: generate code snippets (#912)
    Generate code snippets for Merchants and Merchant Configs pages.
    
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests.

---
+++ src/content/api/_endpoints/merchant-configs-get.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'GET',
+  path: '/api/merchants/5ee0c486308f590260d9a07f/configs/5ee168e8597be5002af7b454',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
---
+++ src/content/api/_endpoints/merchants-get.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'GET',
+  path: '/api/merchants/5ee0c486308f590260d9a07f',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
