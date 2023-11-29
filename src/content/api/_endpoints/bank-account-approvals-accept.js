f42c61133dbedf9c15f77312ca1f90f10023135f
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-11-29 11:33:36 +1100

    ✂️ Bank Account Approvals: generate code snippets (#906)
    Story: https://www.notion.so/centrapay/Use-httpSnippet-to-validate-and-generate-code-blocks-bb9ff3f377f644ccbc2030851af37d27?pvs=4
    
    Test plan: Expect valid cURL requests.

---
+++ src/content/api/_endpoints/bank-account-approvals-accept.js
@@ -0,0 +1,13 @@
+export default {
+  method: 'POST',
+  path: '/api/bank-account-approvals/DcTs3U38HdhfEqwF1GKoT3/accept',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      reason: 'All details match'
+    },
+  },
+};
---
+++ src/content/api/_endpoints/bank-account-approvals-get.js
@@ -0,0 +1,9 @@
+export default {
+  method: 'GET',
+  path: '/api/bank-account-approvals/bbab9a768921019cb856',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+};
