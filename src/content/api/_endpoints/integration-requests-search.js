79285b246dc4b8767ce020da60ccf16fc7c05251
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-12-01 07:54:57 +1100

    ✨ Integrations: Add responses to declarative endpoint files
    Expect page to have valid requests and responses.

--- src/content/api/_endpoints/integration-requests-search.js
+++ src/content/api/_endpoints/integration-requests-search.js
@@ -11,4 +11,23 @@ export default {
       accountId: 'Jaim1Cu1Q55uooxSens6yk'
     }
   },
+  response: {
+    items: [
+      {
+        id: 'DKTs3U38hdhfEqwF1JKoT2',
+        accountId: 'Jaim1Cu1Q55uooxSens6yk',
+        merchantId: '5ee0c486308f590260d9a07f',
+        type: 'epay',
+        status: 'pending',
+        terminalId: '11000021',
+        createdAt: '2020-06-12T01:17:46.499Z',
+        updatedAt: '2020-06-12T01:17:46.499Z',
+        createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        product: {
+          name: 'koha-card'
+        }
+      }
+    ]
+  }
 };
--- src/content/api/_endpoints/managed-integrations-get.js
+++ src/content/api/_endpoints/managed-integrations-get.js
@@ -6,4 +6,23 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: {
+    id: '5ee0c486308f590260d9a07f',
+    type: 'paypal-referral',
+    externalId: 'DKTs3U38hdhfEqwF1JKoT2',
+    enabled: true,
+    params: {
+      centrapayMerchantId: '5ffcaf432003060007b98343',
+      email: 'test@centrapay.com'
+    },
+    status: 'provisioning',
+    claimedByAccountId: 'Jaim1Cu1Q55uooxSens6yk',
+    claimedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    claimedAt: '2020-06-12T01:17:46.499Z',
+    inProgress: true,
+    createdAt: '2020-06-11T01:17:46.499Z',
+    createdBy: 'crn:BIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    updatedAt: '2020-06-12T01:17:46.499Z',
+    updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+  }
 };
