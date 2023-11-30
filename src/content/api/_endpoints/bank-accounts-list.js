91d3ef91fc17c1fb5e903d5aecd575aaff69139a
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-30 13:47:47 +1100

    ✨ Bank Accounts: Add responses to declarative endpoint files
    Expect page to have valid requests and responses.

--- src/content/api/_endpoints/bank-accounts-list.js
+++ src/content/api/_endpoints/bank-accounts-list.js
@@ -6,4 +6,47 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: [
+    {
+      id: 'XZbPLViMzekVBbF7QMqgaY',
+      accountId: 'Jaim1Cu1Q55uooxSens6yk',
+      status: 'created',
+      bankAccountNumber: '02-0500-0568903-097',
+      bankAccountName: 'Pocket Money',
+      directDebitAuthorized: false,
+      bankRegion: 'nz',
+      createdBy: 'crn::user:57142ecc-e5e4-456a-8312-1fad4fdef3c7',
+      createdAt: '2022-04-19T05:43:40.425Z',
+      verified: false,
+      modifiedBy: 'crn::user:57142ecc-e5e4-456a-8312-1fad4fdef3c7',
+      modifiedAt: '2022-04-19T05:43:40.425Z',
+      approvals: [
+
+      ],
+      type: 'quartz',
+      test: true
+    },
+    {
+      id: '3Kfdm8cuW1W6f8AoWJREs4',
+      accountId: 'Jaim1Cu1Q55uooxSens6yk',
+      status: 'created',
+      bankAccountNumber: '00-1213-1231299-999',
+      bankAccountName: 'Jean',
+      directDebitAuthorized: false,
+      bankRegion: 'nz',
+      createdBy: 'crn::user:57142ecc-e5e4-456a-8312-1fad4fdef3c7',
+      createdAt: '2022-02-22T03:27:57.138Z',
+      verified: false,
+      modifiedBy: 'crn::user:57142ecc-e5e4-456a-8312-1fad4fdef3c7',
+      modifiedAt: '2022-02-22T03:27:57.138Z',
+      approvals: [
+        {
+          type: 'settlement',
+          status: 'pending',
+          updatedAt: '2021-11-08T21:52:39.915Z'
+        }
+      ],
+      type: 'centrapay'
+    }
+  ]
 };
--- src/content/api/_endpoints/bank-authorities-create.js
+++ src/content/api/_endpoints/bank-authorities-create.js
@@ -15,4 +15,18 @@ export default {
       bankAccountName: 'John Doe'
     },
   },
+  response: {
+    id: 'WRhAxxWpTKb5U7pXyxQjjY',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    bankAccountNumber: '12-1234-1234567-123',
+    bankAccountName: 'John Doe',
+    status: 'created',
+    verified: false,
+    directDebitAuthorized: true,
+    createdAt: '2020-06-12T01:17:46.499Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    modifiedAt: '2020-06-12T01:17:46.499Z',
+    modifiedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    approvals: []
+  }
 };
