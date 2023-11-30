82a1104012ebe7a5a7aa1c21f5275f37ecdc3015
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-12-01 09:35:11 +1100

    ✨ Bank Accounts: Add responses to declarative endpoint files (#927)
    Expect page to have valid requests and responses.

--- src/content/api/_endpoints/bank-authorities-list.js
+++ src/content/api/_endpoints/bank-authorities-list.js
@@ -6,4 +6,36 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: [
+    {
+      id: 'WRhAxxWpTKb5U7pXyxQjjY',
+      accountId: 'Jaim1Cu1Q55uooxSens6yk',
+      bankAccountNumber: '12-1234-1234567-123',
+      bankAccountName: 'John Doe',
+      status: 'created',
+      verified: false,
+      directDebitAuthorized: true,
+      createdAt: '2020-06-12T01:17:46.499Z',
+      approvals: [
+
+      ]
+    },
+    {
+      id: 'b5URhAxxWpTKyxQjjY7pXW',
+      accountId: 'Jaim1Cu1Q55uooxSens6yk',
+      bankAccountNumber: '12-1234-1234567-123',
+      bankAccountName: 'Jane Doe',
+      status: 'active',
+      verified: true,
+      directDebitAuthorized: true,
+      createdAt: '2020-06-12T01:17:46.499Z',
+      approvals: [
+        {
+          type: 'settlement',
+          status: 'pending',
+          updatedAt: '2021-11-08T21:52:39.915Z'
+        }
+      ]
+    }
+  ]
 };
