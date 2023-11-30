878f50ec5391f74406f7319ceb285b99a602b49c
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-12-01 09:41:11 +1100

    ✨ Merchants: Add responses to declarative endpoint files
    Expect merchant and merchant configs pages to have valid requests and responses.

--- src/content/api/_endpoints/merchant-configs-create.js
+++ src/content/api/_endpoints/merchant-configs-create.js
@@ -23,4 +23,26 @@ export default {
       ]
     },
   },
+  response: {
+    id: '5ee168e8597be5002af7b454',
+    merchantId: '5ee0c486308f590260d9a07f',
+    createdAt: '2021-11-12T01:17:46.499Z',
+    updatedAt: '2021-11-12T01:17:46.499Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    paymentOptions: [
+      {
+        type: 'centrapay.nzd.main',
+        walletId: '1234c486308f3f0a23f0f92b'
+      },
+      {
+        type: 'epay.nzd.main',
+        terminalId: '11000021'
+      },
+      {
+        type: 'farmlands.nzd.main',
+        farmlandsMerchantNumber: 'DbgY2SyD5M85zkePJjsQEf'
+      }
+    ]
+  }
 };
--- src/content/api/_endpoints/merchants-update.js
+++ src/content/api/_endpoints/merchants-update.js
@@ -20,4 +20,26 @@ export default {
       }
     },
   },
+  response: {
+    id: '5ee0c486308f590260d9a07f',
+    accountId: 'yqwyya0rzz3vvshqw0474u89xtj5fn',
+    name: 'Centrapay Café',
+    test: false,
+    country: 'NZ',
+    onboardingStatus: 'applied',
+    location: {
+      lat: '-36.8483579',
+      lng: '174.7725834',
+      city: 'Auckland',
+      postCode: '1010',
+      country: 'NZ'
+    },
+    settlementConfig: {
+      bankAccountId: 'WRhAxxWpTKb5U7pXyxQjjY'
+    },
+    createdAt: '2021-09-12T01:11:22.491Z',
+    updatedAt: '2021-11-12T01:17:46.499Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+  }
 };
