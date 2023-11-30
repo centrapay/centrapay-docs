b34f135bbb2b2c05486e4d9c512e6cb37c1f2cb6
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-30 14:08:45 +1100

    ✨ Funds Transfers: Add responses to declarative endpoint files
    Expect page to have valid requests and responses.

--- src/content/api/_endpoints/topups-get.js
+++ src/content/api/_endpoints/topups-get.js
@@ -6,4 +6,15 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: {
+    id: 'WRhAxxWpTKb5U7pXyxQjjY',
+    assetId: 'Te2uDM7xhDLWGVJU3nzwnh',
+    bankAccountId: 'FRhAzzWpTKb5U7pZygQjjY',
+    accountId: 'aBc932S9182qwCDqwer',
+    type: 'topup',
+    amount: '10000',
+    status: 'created',
+    createdAt: '2020-05-01T12:30:00.000Z',
+    updatedAt: '2020-05-01T12:30:00.000Z'
+  }
 };
--- src/content/api/_endpoints/topups-list-by-accountId.js
+++ src/content/api/_endpoints/topups-list-by-accountId.js
@@ -6,4 +6,28 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: [
+    {
+      id: '5thg2RPBZEfYTPJdQ63Cre',
+      assetId: 'Te2uDM7xhDLWGVJU3nzwnh',
+      bankAccountId: 'FRhAzzWpTKb5U7pZygQjjY',
+      accountId: 'aBc932S9182qwCDqwer',
+      type: 'topup',
+      amount: '10000',
+      status: 'created',
+      createdAt: '2020-05-01T12:30:00.000Z',
+      updatedAt: '2020-05-01T12:30:00.000Z'
+    },
+    {
+      id: 'hg2RfYTQ635tPBZEPJdCre',
+      assetId: 'Te2uDM7xhDLWGVJU3nzwnh',
+      bankAccountId: 'FRhAzzWpTKb5U7pZygQjjY',
+      accountId: 'aBc932S9182qwCDqwer',
+      type: 'topup',
+      amount: '10000',
+      status: 'created',
+      createdAt: '2020-05-01T12:30:00.000Z',
+      updatedAt: '2020-05-01T12:30:00.000Z'
+    }
+  ]
 };
