7c90718529df3d3f1fd59374f0e76dbc80d65619
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-30 11:11:01 +1100

    API keys

--- src/content/api/_endpoints/api-keys-create.js
+++ src/content/api/_endpoints/api-keys-create.js
@@ -11,4 +11,16 @@ export default {
       role: 'merchant-terminal'
     },
   },
+  response: {
+    name: 'MyAPIkey',
+    createdAt: '2020-06-01T22:32:56.631Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:AdminKey',
+    modifiedAt: '2020-06-01T22:32:56.631Z',
+    modifiedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:AdminKey',
+    enabled: true,
+    role: 'merchant-terminal',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    version: '1',
+    secret: 'EoaEL7skkedBBy9MzrBSyxG95vUAKjYkiFvWEfiAx'
+  }
 };
