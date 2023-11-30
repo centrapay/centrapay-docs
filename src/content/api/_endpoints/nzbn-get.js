1c066934c4f3844a3459b2a8622fd25ae1896b0f
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-30 13:29:05 +1100

    ✨ Accounts: Add responses to declarative endpoint files
    Expect page to have valid requests and responses.

--- src/content/api/_endpoints/nzbn-get.js
+++ src/content/api/_endpoints/nzbn-get.js
@@ -6,4 +6,30 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: {
+    nzbn: '9429046246448',
+    companyName: 'CENTRAPAY LIMITED',
+    tradingName: 'CentraPay',
+    companyNumber: '6340244',
+    directors: [
+      {
+        firstName: 'John',
+        lastName: 'DOE',
+        addressLines: [
+          '7 Tara Street',
+          'Downmore',
+          'Auckland'
+        ]
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'DOE',
+        addressLines: [
+          '82 Greatwood Road',
+          'Northclover',
+          'Auckland'
+        ]
+      }
+    ]
+  }
 };
