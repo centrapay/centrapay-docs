0853afe6ba3fe05c8732222967ad9fa2c048bca2
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-12-01 09:39:06 +1100

    ✨ Bank Account Approvals: Add responses to declarative endpoint files (#928)
    Expect page to have valid requests and responses.

--- src/content/api/_endpoints/bank-account-approvals-create.js
+++ src/content/api/_endpoints/bank-account-approvals-create.js
@@ -12,4 +12,16 @@ export default {
       type: 'settlement'
     },
   },
+  response: {
+    id: 'DcTs3U38HdhfEqwF1GKoT3',
+    mediaUploadId: 'uooxSens6ykJaim1Cu1Q55',
+    bankAccountId: 'WRhAxxWpTKb5U7pXyxQjjY',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    type: 'settlement',
+    status: 'created',
+    createdAt: '2021-11-08T21:52:39.915Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    modifiedAt: '2021-11-08T21:52:39.915Z',
+    modifiedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+  }
 };
