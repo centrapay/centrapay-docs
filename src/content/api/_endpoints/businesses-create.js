9062dd0000a316913a54e34f5d5ac7d9f1c64f2d
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-11-30 12:16:30 +1100

    Businesses page

--- src/content/api/_endpoints/businesses-create.js
+++ src/content/api/_endpoints/businesses-create.js
@@ -14,4 +14,22 @@ export default {
       }
     },
   },
+  response: {
+    id: 'DKTs3U38hdhfEqwF1JKoT2',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    accountName: 'Centrapay',
+    nzbn: '9429046246448',
+    name: 'CENTRAPAY LIMITED',
+    tradingName: 'CentraPay',
+    companyNumber: '6340244',
+    createdAt: '2020-06-12T01:17:46.499Z',
+    updatedAt: '2020-06-12T01:17:46.499Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    taxNumber: {
+      value: '123-456-789',
+      type: 'nz-gst',
+    },
+    onboardingStatus: 'applied'
+  }
 };
