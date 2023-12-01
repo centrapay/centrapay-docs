c723e35b218d8b9812f18072c65d906345cd554e
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-12-01 14:20:00 +1100

    ✨ Scanned codes: Add responses to declarative endpoint files (#938)
    Expect all endpoint examples to have valid response examples.

--- src/content/api/_endpoints/patron-codes-get.js
+++ src/content/api/_endpoints/patron-codes-get.js
@@ -6,4 +6,12 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: {
+    id: 'V17FByEP9gm1shSG6a1Zzx',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    barcode: '9990001234567895',
+    createdAt: '2021-06-08T22:55:00.000Z',
+    expiresAt: '2021-06-08T23:00:00.000Z',
+    appName: 'centrapay'
+  }
 };
