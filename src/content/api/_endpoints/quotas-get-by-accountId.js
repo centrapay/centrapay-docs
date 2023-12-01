25761e211c94575a873bdc7f167ab7a7db3a1446
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-12-01 14:17:55 +1100

    ✨ Quotas: Add responses to declarative endpoint files (#937)
    Expect page to have valid requests and responses.
    Note layout changed as per screenshot.

--- src/content/api/_endpoints/quotas-get-by-accountId.js
+++ src/content/api/_endpoints/quotas-get-by-accountId.js
@@ -6,4 +6,38 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: [
+    {
+      limit: '1000',
+      interval: '2021-03',
+      period: 'monthly',
+      usage: '500',
+      type: 'spend',
+      assetType: 'centrapay.nzd.main'
+    },
+    {
+      limit: '9999',
+      interval: '2021',
+      period: 'yearly',
+      usage: '1555',
+      type: 'spend',
+      assetType: 'centrapay.nzd.main'
+    },
+    {
+      limit: '1000',
+      interval: '2021-03',
+      period: 'monthly',
+      usage: '500',
+      type: 'topup',
+      assetType: 'centrapay.nzd.main'
+    },
+    {
+      limit: '9999',
+      interval: '2021',
+      period: 'yearly',
+      usage: '5000',
+      type: 'topup',
+      assetType: 'centrapay.nzd.main'
+    }
+  ]
 };
