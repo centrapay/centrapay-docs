9a47a4093ab71f3c9a5bc4b3298deb3c4ab52e41
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-12-01 13:02:28 +1100

    ✨ Settlements: Add responses to declarative endpoint files
    Expect to see valid response.

--- src/content/api/_endpoints/settlements-list-by-merchantId.js
+++ src/content/api/_endpoints/settlements-list-by-merchantId.js
@@ -6,4 +6,30 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: {
+    items: [
+      {
+        id: '89028sh9308f590260d9a07f',
+        status: 'confirmed',
+        assetType: 'centrapay.nzd.main',
+        currency: 'NZD',
+        createdAt: '2021-11-12T01:17:46.499Z',
+        createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        settledAt: '2021-11-13T11:59:59.999Z',
+        settledBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        settlementAmount: '2500',
+        settlementBankAccountId: '67e0c486308f590260d9a139'
+      },
+      {
+        id: '9ds2cs89028sh90260d9f91h',
+        status: 'awaiting-confirmation',
+        assetType: 'centrapay.nzd.main',
+        currency: 'NZD',
+        createdAt: '2021-11-13T01:17:46.499Z',
+        createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        settlementAmount: '2200',
+        settlementBankAccountId: '67e0c486308f590260d9a139'
+      }
+    ]
+  }
 };
