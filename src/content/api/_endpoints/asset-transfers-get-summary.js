afb7c4769419ec56b3acc2b8cc73e7db9b1d0088
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-12-04 09:41:02 +1100

    ✨ Asset Transfers: Add responses to declarative endpoint files (#942)
    Expect valid responses for all API examples.

--- src/content/api/_endpoints/asset-transfers-get-summary.js
+++ src/content/api/_endpoints/asset-transfers-get-summary.js
@@ -1,4 +1,13 @@
 export default {
   method: 'GET',
   path: '/api/asset-transfers/M7Kn2stAxNa6ri7h/summary',
+  response: {
+    id: 'M7Kn2stAxNa6ri7h',
+    status: 'created',
+    assetId: 'YGRo6TYYSxH3js7',
+    message: 'Happy birthday',
+    senderName: 'My Cafe',
+    createdAt: '2020-05-01T12:30:00.000Z',
+    url: 'https://app.centrapay.com/transfer/M7Kn2stAxNa6ri7h'
+  }
 };
