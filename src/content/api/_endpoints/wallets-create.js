1885a4b4dbd7e339b089aba3634109e983e10777
Megan Steenkamp <64108933+MeganSteenkamp@users.noreply.github.com>
2023-12-04 09:29:13 +1100

    ✨ Wallets: Add responses to declarative endpoint files (#945)
    Expect valid API responses for each endpoint.

--- src/content/api/_endpoints/wallets-create.js
+++ src/content/api/_endpoints/wallets-create.js
@@ -12,4 +12,18 @@ export default {
       settlement: 'true'
     },
   },
+  response: {
+    id: 'WRhAxxWpTKb5U7pXyxQjjY',
+    accountId: 'Te2uDM7xhDLWGVJU3nzwnh',
+    category: 'money',
+    type: 'centrapay.nzd.main',
+    liveness: 'main',
+    description: 'NZD',
+    createdAt: '2021-01-01T00:00:00.000Z',
+    status: 'active',
+    currency: 'NZD',
+    balance: '0',
+    availableBalance: '6000',
+    settlement: true
+  }
 };
