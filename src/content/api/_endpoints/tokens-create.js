d0dd02a90cee30dd516f7492966af0eed53ec1fb
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-12-04 09:11:33 +1100

    ✨ Tokens: Add responses to declarative endpoint files
    Expect valid API responses for all endpoints.

--- src/content/api/_endpoints/tokens-create.js
+++ src/content/api/_endpoints/tokens-create.js
@@ -12,4 +12,23 @@ export default {
       externalId: '23403283262'
     },
   },
+  response: {
+    id: 'pe32dd90-b46c-11ea-92828sa',
+    accountId: 'WRhAxxWpTKb5U7pXyxQjjP',
+    category: 'token',
+    collectionId: 'Jaim1Cu1Q55uooxSens6yk',
+    status: 'active',
+    createdAt: '2021-01-17T18:00:23.000Z',
+    activeFrom: '2021-01-17T18:00:23.000Z',
+    expiresAt: '2022-01-18T18:00:23.000Z',
+    liveness: 'test',
+    createdBy: 'crn:1234abc:api-key:MyAssetIssuerKey',
+    description: 'My Cafe Token',
+    issuerImg: 'https://static.centrapay.com/assets/brands/centraperk/logo.png',
+    img: 'https://static.centrapay.com/assets/brands/centraperk/cafe-token.png',
+    issuer: 'Centraperk Cafe',
+    issuerWebsite: 'www.centraperk-cafe.com',
+    type: 'centrapay.token.test',
+    externalId: '23403283262'
+  }
 };
