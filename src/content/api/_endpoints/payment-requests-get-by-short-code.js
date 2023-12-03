bc5f26f1b63ed6871069d357485fc0323a79f279
Megan Steenkamp <megan.steenkamp@centrapay.com>
2023-12-04 08:59:02 +1100

    ✨ Payment Requests: Add responses to declarative endpoint files
    Expect valid request and response pairs for payment requests page and legacy payment requests page.

--- src/content/api/_endpoints/payment-requests-get-by-short-code.js
+++ src/content/api/_endpoints/payment-requests-get-by-short-code.js
@@ -6,4 +6,33 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
   },
+  response: {
+    id: 'MhocUmpxxmgdHjr7DgKoKw',
+    shortCode: 'CP-C7F-ZS5',
+    url: 'https://app.centrapay.com/pay/MhocUmpxxmgdHjr7DgKoKw',
+    patronCodeId: 'V17FByEP9gm1shSG6a1Zzx',
+    barcode: '9990001234567895',
+    merchantId: '26d3Cp3rJmbMHnuNJmks2N',
+    merchantName: 'Centrapay Café',
+    configId: '5efbe2fb96c08357bb2b9242',
+    value: {
+      currency: 'NZD',
+      amount: '100'
+    },
+    paymentOptions: [
+      {
+        amount: '100',
+        assetType: 'centrapay.nzd.test'
+      }
+    ],
+    merchantConditions: [
+
+    ],
+    status: 'new',
+    createdAt: '2021-06-08T04:04:27.426Z',
+    updatedAt: '2021-06-08T04:04:27.426Z',
+    expiresAt: '2021-06-08T04:06:27.426Z',
+    liveness: 'test',
+    expirySeconds: 120
+  }
 };
