0811998f4b18e84c4a7aea4e91507bd064e55bbc
Ching-Wen Hu <chingwen.hu@centrapay.com>
2024-02-06 13:00:22 +0800

    Add APIs for create a program and list programs

---
+++ src/content/api/_endpoints/programs-list-by-accountId.js
@@ -0,0 +1,39 @@
+export default {
+  method: 'GET',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/programs',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+  response: {
+    items: [
+      {
+        id: 'WRhAxxWpTKb5U7pXyxQjjY',
+        name: 'Centrapay Cafe Loyalty Program',
+        type: 'loyalty',
+        description: 'Centrapay Cafe Loyalty: Earn rewards, enjoy perks! Join now!',
+        mediaUploadId: '8aoMfscvtuewsuJzmzBzAs',
+        img: "https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743",
+        accountId: 'Jaim1Cu1Q55uooxSens6yk',
+        brandId: 'EMbBZX4SbwqA1Ra6kkCNES',
+        createdAt: '2021-08-25T00:02:49.488Z',
+        createdBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+        test: true
+      },
+      {
+        id: 'DKTs3U38hdhfEqwF1JKoT2',
+        name: 'Centra Tea Warehouse Loyalty Program',
+        type: 'loyalty',
+        description: 'Centra Tea Warehouse Loyalty: Earn rewards, enjoy perks! Join now!',
+        mediaUploadId: 'Hopo4g34sLVdjEMBs2p19F',
+        img: "https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743",
+        accountId: 'Jaim1Cu1Q55uooxSens6yk',
+        brandId: 'JKKDMU38hd01hfEqwF1oT2',
+        createdAt: '2021-08-25T00:02:49.488Z',
+        createdBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+        test: true
+      }
+    ]
+  }
+};
