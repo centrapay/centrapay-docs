9560c7883aca0cfab38fb0ef2382c06ca3b436af
Wulingoh <32785419+Wulingoh@users.noreply.github.com>
2024-02-12 15:09:43 -0700

    update bank accounts get balance doc (#965)
    * update bank accounts get balance doc
    
    * remove error codes from get bank balance doc

--- src/content/api/_endpoints/bank-accounts-get-balance.js
+++ src/content/api/_endpoints/bank-accounts-get-balance.js
@@ -1,14 +1,21 @@
 export default {
   method: 'GET',
-  path: '/api/bank-accounts/WRhAxxWpTKb5U7pXyxQjjY/balance',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/bank-account-balances',
   request: {
     headers: {
       'X-Api-Key': '<TOKEN>',
     },
   },
   response: {
-    bankAccountId: 'WRhAxxWpTKb5U7pXyxQjjY',
-    balance: 1000,
-    currency: 'NZD'
+    items: [
+      {
+        bankAccountId: 'b3f9ba1abff64775877d4a2fb073b123',
+        bankAccountNumber: '12-1234-1234567-123',
+        balance: '101.00',
+        availableBalance: '101.00',
+        test: true
+      }
+    ],
+    nextPageKey: '48a16140-24d2-4aec-b52f-a9d4f8f23876'
   }
 };
