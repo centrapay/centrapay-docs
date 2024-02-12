3ac1af6964a11aa4a301b1a36ab1fdea2ad5de26
Michaeline Goh <michaeline.goh@centrapay.com>
2024-02-13 10:13:09 +1300

    remove error codes from get bank balance doc

--- src/content/api/_endpoints/bank-accounts-get-balance.js
+++ src/content/api/_endpoints/bank-accounts-get-balance.js
@@ -12,9 +12,10 @@ export default {
         bankAccountId: 'b3f9ba1abff64775877d4a2fb073b123',
         bankAccountNumber: '12-1234-1234567-123',
         balance: '101.00',
-        availableBalance: '101.00'
+        availableBalance: '101.00',
+        test: true
       }
     ],
-    nextPageKey: ''
+    nextPageKey: '48a16140-24d2-4aec-b52f-a9d4f8f23876'
   }
 };
