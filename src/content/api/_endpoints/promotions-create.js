3f1e4547d86b55cb2cfd9968cb0cf18c880784bf
Raymond Yang <raymondyang@outlook.co.nz>
2024-02-16 11:02:12 +0800

    Fix lint error single quotes

--- src/content/api/_endpoints/promotions-create.js
+++ src/content/api/_endpoints/promotions-create.js
@@ -6,63 +6,63 @@ export default {
       'X-Api-Key': '<TOKEN>',
     },
     payload: {
-      name: "Spend a Buck",
-      description: "Make a payment of $1 or more",
-      mediaUploadId: "26yUWG6wFgmva8UaDiCTWq",
-      startsAt: "2023-02-16T00:47:54.131Z",
-      endsAt: "2024-02-16T00:47:54.131Z",
+      name: 'Spend a Buck',
+      description: 'Make a payment of $1 or more',
+      mediaUploadId: '26yUWG6wFgmva8UaDiCTWq',
+      startsAt: '2023-02-16T00:47:54.131Z',
+      endsAt: '2024-02-16T00:47:54.131Z',
       rewards: [
         {
-          assetType: "centrapay.nzd.main",
-          amount: "500",
+          assetType: 'centrapay.nzd.main',
+          amount: '500',
         }
       ],
-      eventType: "payment",
+      eventType: 'payment',
       target: {
-        type: "count",
+        type: 'count',
         amount: 1
       },
-      type: "challenge",
+      type: 'challenge',
       conditions: [
         {
-          field: "amount",
-          value: "99",
-          operator: "greater-than"
+          field: 'amount',
+          value: '99',
+          operator: 'greater-than'
         }
       ]
     },
   },
   response: {
-    id: "8aoMfscvtuewsuJzmzBzAs",
-    accountId: "57SyRRgZ1U8Kq2wKpCnSR5",
-    programId: "WRhAxxWpTKb5U7pXyxQjjY",
-    name: "Spend a Buck",
-    description: "Make a payment of $1 or more",
-    mediaUploadId: "26yUWG6wFgmva8UaDiCTWq",
-    startsAt: "2023-02-16T00:47:54.131Z",
-    endsAt: "2024-02-16T00:47:54.131Z",
+    id: '8aoMfscvtuewsuJzmzBzAs',
+    accountId: '57SyRRgZ1U8Kq2wKpCnSR5',
+    programId: 'WRhAxxWpTKb5U7pXyxQjjY',
+    name: 'Spend a Buck',
+    description: 'Make a payment of $1 or more',
+    mediaUploadId: '26yUWG6wFgmva8UaDiCTWq',
+    startsAt: '2023-02-16T00:47:54.131Z',
+    endsAt: '2024-02-16T00:47:54.131Z',
     rewards: [
       {
-        assetType: "centrapay.nzd.main",
-        amount: "500",
+        assetType: 'centrapay.nzd.main',
+        amount: '500',
       }
     ],
-    eventType: "payment",
+    eventType: 'payment',
     target: {
-      type: "count",
+      type: 'count',
       amount: 1
     },
-    type: "challenge",
+    type: 'challenge',
     conditions: [
       {
-        field: "amount",
-        value: "99",
-        operator: "greater-than"
+        field: 'amount',
+        value: '99',
+        operator: 'greater-than'
       }
     ],
-    createdAt: "2023-02-08T04:04:27.426Z",
-    createdBy: "crn::user:1234",
-    updatedAt: "2023-02-08T04:04:27.426Z",
-    updatedBy: "crn::user:1234",
+    createdAt: '2023-02-08T04:04:27.426Z',
+    createdBy: 'crn::user:1234',
+    updatedAt: '2023-02-08T04:04:27.426Z',
+    updatedBy: 'crn::user:1234',
   },
 };
