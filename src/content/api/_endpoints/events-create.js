16c090086f3aa7eed02ae0e731969c7887b4587a
Ching-Wen Hu <chingwen.hu@centrapay.com>
2024-03-20 06:48:45 +0800

    Address feedback and Update wording

--- src/content/api/_endpoints/events-create.js
+++ src/content/api/_endpoints/events-create.js
@@ -7,11 +7,10 @@ export default {
       'Content-Type': 'application/json',
     },
     payload: {
-      type: 'payment:paid',
-      idempotencyKey: 'payment-de32dd90-b46c-11ea-93c3-83a333b86e7b',
+      type: 'user-login',
+      idempotencyKey: 'login-de32dd90-b46c-11ea-93c3-83a333b86e7b',
       data: {
-        'foo': 'bar',
-        'fizzbuzz': 2
+        'userId': 'b657195e-dc2f-11ea-8566-e7710d592c99-123',
       },
       test: true
     },
