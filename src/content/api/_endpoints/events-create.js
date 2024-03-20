3b81aafe0e83a2c83bf06014544df2096ae57e9d
Ching-Wen Hu <chingwen.hu@centrapay.com>
2024-03-20 09:45:18 +0800

    Update event api response

--- src/content/api/_endpoints/events-create.js
+++ src/content/api/_endpoints/events-create.js
@@ -16,6 +16,15 @@ export default {
     },
   },
   response: {
-    statusCode: 200,
+    id: 'L75M3L56N2PtBSt8g7uXLU',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    type: 'user-login',
+    data: {
+      'userId': 'b657195e-dc2f-11ea-8566-e7710d592c99-123',
+    },
+    idempotencyKey: 'login-de32dd90-b46c-11ea-93c3-83a333b86e7b',
+    test: true,
+    createdAt: '2020-05-01T12:30:00.000Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
   }
 };
