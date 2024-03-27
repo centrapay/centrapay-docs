48d2bfb4709ab162b406f156d9bc2923e4f6c347
Michaeline Goh <32785419+Wulingoh@users.noreply.github.com>
2024-03-27 17:53:43 -0600

    feat: add request asset transfer endpoint doc (#991)
    * feat: add request asset transfer endpoint doc
    
    * add experimental badge
    
    * reword the recipient and sender wording
    
    * remove senderName from endpoint

---
+++ src/content/api/_endpoints/asset-transfers-request.js
@@ -0,0 +1,29 @@
+export default {
+  method: 'POST',
+  path: '/api/asset-transfer-requests',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      recipientAlias: '12-1234-1234567-123',
+      senderAccountId: '9EDxUT91TMsUjoqoQeBuLQ',
+      value: '1000',
+      assetType: 'quartz.nzd.test'
+    },
+  },
+  response: {
+    id: 'M7Kn2stAxNa6ri7h',
+    recipientAlias: '12-1234-1234567-123',
+    status: 'requested',
+    assetType: 'quartz.nzd.main',
+    description: 'request payment',
+    senderAccountId: '9EDxUT91TMsUjoqoQeBuLQ',
+    createdAt: '2024-03-01T23:56:21.514Z',
+    createdBy: 'crn:userId:1234',
+    suppressNotifications: false,
+    value: '1000',
+    message: 'Payment request',
+  }
+};
