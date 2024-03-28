119bed3159209fe8b948e726b109b38b4e2f8861
Braden Palmer <braden.palmer@centrapay.com>
2024-03-28 13:22:27 +1300

    add docs for accept asset transfer request endpoint

---
+++ src/content/api/_endpoints/asset-transfer-requests-accept.js
@@ -0,0 +1,25 @@
+export default {
+  method: 'POST',
+  path: '/api/asset-transfers-requests/M7Kn2stAxNa6ri7h/accept',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      assetId: 'YGRo6TYYSxH3js7',
+      value: '1000',
+      senderName: 'Jane Doe'
+    },
+  },
+  response: {
+    assetTransferId: 'M7Kn2stAxNa6ri7h',
+    status: 'accepted',
+    type: 'asset-transfer:accepted',
+    createdAt: '2020-05-02T01:03:37.222Z',
+    createdBy: 'sdasd98199dadas',
+    assetId: 'YGRo6TYYSxH3js7',
+    senderName: 'Jane Doe',
+    value: '1000',
+  }
+};
