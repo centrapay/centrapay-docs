1d9003c2f43c5a2e993ed42313b0cf394d6d277e
Chingwen Hu <cwhu823@icloud.com>
2024-04-08 07:31:18 +0800

    Add create loyalty program memberships API (#994)
    * Add create loyalty program memberships API

---
+++ src/content/api/_endpoints/loyalty-program-memberships-create.js
@@ -0,0 +1,26 @@
+export default {
+  method: 'POST',
+  path: '/api/loyalty-programs/WRhAxxWpTKb5U7pXyxQjjY/memberships',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    },
+  },
+  response: {
+    id: 'L75M3L56N2PtBSt8g7uXLU',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    programId: 'WRhAxxWpTKb5U7pXyxQjjY',
+    name: 'Centrapay Cafe Loyalty Program',
+    description: 'Centrapay Cafe Loyalty: Earn rewards, enjoy perks! Join now!',
+    status: 'active',
+    mediaUploadId: '8aoMfscvtuewsuJzmzBzAs',
+    img: 'https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743',
+    createdAt: '2021-08-25T00:02:49.488Z',
+    createdBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+    test: true
+  }
+};
