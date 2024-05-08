1503ec6c8040b74e009cf99d9d5c98d808e5fa16
Raymond Yang <raymondyang@outlook.co.nz>
2024-05-08 13:55:56 +1200

    Add conditionsDescription to the Promotion model (#1005)

--- src/content/api/_endpoints/promotions-create.js
+++ src/content/api/_endpoints/promotions-create.js
@@ -8,7 +8,7 @@ export default {
     },
     payload: {
       name: 'Spend a Buck',
-      description: 'Make a payment of $1 or more',
+      description: 'Make a payment',
       mediaUploadId: '26yUWG6wFgmva8UaDiCTWq',
       startsAt: '2023-02-16T00:47:54.131Z',
       endsAt: '2024-02-16T00:47:54.131Z',
@@ -30,7 +30,8 @@ export default {
           value: '99',
           operator: 'greater-than'
         }
-      ]
+      ],
+      conditionsDescription: 'The amount of the payment must be greater than $1.'
     },
   },
   response: {
@@ -38,7 +39,7 @@ export default {
     accountId: '57SyRRgZ1U8Kq2wKpCnSR5',
     programId: 'WRhAxxWpTKb5U7pXyxQjjY',
     name: 'Spend a Buck',
-    description: 'Make a payment of $1 or more',
+    description: 'Make a payment',
     mediaUploadId: '26yUWG6wFgmva8UaDiCTWq',
     img: 'https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743',
     startsAt: '2023-02-16T00:47:54.131Z',
@@ -62,6 +63,7 @@ export default {
         operator: 'greater-than'
       }
     ],
+    conditionsDescription: 'The amount of the payment must be greater than $1.',
     createdAt: '2023-02-08T04:04:27.426Z',
     createdBy: 'crn::user:1234',
     updatedAt: '2023-02-08T04:04:27.426Z',
