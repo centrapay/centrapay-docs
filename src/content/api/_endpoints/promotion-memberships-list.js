7708e8c5d5600ebcdfcd06108bb4548aa7c7dbfe
Chingwen Hu <cwhu823@icloud.com>
2024-05-10 10:07:20 +0800

    Update the field names (#1007)
    * Update the field names

--- src/content/api/_endpoints/promotion-memberships-list.js
+++ src/content/api/_endpoints/promotion-memberships-list.js
@@ -13,7 +13,7 @@ export default {
         accountId: '57SyRRgZ1U8Kq2wKpCnSR5',
         programId: 'WRhAxxWpTKb5U7pXyxQjjY',
         name: 'Spend a Buck',
-        description: 'Make a payment of $1 or more',
+        summary: 'Make a payment',
         mediaUploadId: '26yUWG6wFgmva8UaDiCTWq',
         img: 'https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743',
         startsAt: '2023-02-16T00:47:54.131Z',
@@ -37,6 +37,7 @@ export default {
             operator: 'greater-than'
           }
         ],
+        description: 'The amount of the payment must be greater than $1.',
         createdAt: '2023-02-08T04:04:27.426Z',
         createdBy: 'crn::user:1234',
         modifiedAt: '2023-02-08T04:04:27.426Z',
