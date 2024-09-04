25a495d7e5bfb32664d26fb862e84f2c9a702d50
Diego Pupato <diego.pupato@centrapay.com>
2024-09-04 12:41:04 +1200

    feat: add list managed integration by account id

---
+++ src/content/api/_endpoints/managed-integrations-list-by-accountId.js
@@ -0,0 +1,50 @@
+export default {
+  method: 'GET',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/managed-integrations',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+  response: [
+    {
+      id: '5ee0c486308f590260d9a07f',
+      type: 'paypal-referral',
+      externalId: 'DKTs3U38hdhfEqwF1JKoT2',
+      enabled: true,
+      params: {
+        centrapayMerchantId: '5ffcaf432003060007b98343',
+        email: 'test@centrapay.com'
+      },
+      status: 'provisioning',
+      claimedByAccountId: 'Jaim1Cu1Q55uooxSens6yk',
+      claimedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+      claimedAt: '2020-06-12T01:17:46.499Z',
+      inProgress: true,
+      createdAt: '2020-06-11T01:17:46.499Z',
+      createdBy: 'crn:BIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+      updatedAt: '2020-06-12T01:17:46.499Z',
+      updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+    },
+    {
+      id: '5ee0c486308f590260d9a07f',
+      type: 'payap',
+      externalId: 'DKTs3U38hdhfEqwF1JKoT2',
+      enabled: true,
+      params: {
+        nzbn: '12345657',
+        companyName: 'Centrapay',
+        tradingName: 'Centrapay'
+      },
+      status: 'provisioning',
+      claimedByAccountId: 'Jaim1Cu1Q55uooxSens6yk',
+      claimedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+      claimedAt: '2020-06-12T01:17:46.499Z',
+      inProgress: true,
+      createdAt: '2020-06-11T01:17:46.499Z',
+      createdBy: 'crn:BIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+      updatedAt: '2020-06-12T01:17:46.499Z',
+      updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+    }
+  ]
+};
