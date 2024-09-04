d9454a5e3b84ea9f37966a13a9f90db080a51920
Diego Pupato <pupato.diego@gmail.com>
2024-09-04 16:08:30 +1200

    Add list managed integration by account id (#1053)
    * feat: add list managed integration by account id
    
    * fix: fix grammar
    
    * fix: change from payap to paypal-referral integration type
    
    * refactor: add pageKey to list doc
    
    * test: add snapshot

---
+++ src/content/api/_endpoints/managed-integrations-list-by-accountId.js
@@ -0,0 +1,55 @@
+export default {
+  method: 'GET',
+  path: '/api/accounts/Jaim1Cu1Q55uooxSens6yk/managed-integrations',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+    queryString: {
+      pageKey: 'Collection#k9oBPAOgjz4k6a9MXNfRlFzn|#Collection|4xb2jbtvgr07eax9259od5he'
+    }
+  },
+  response: {
+    nextPageKey: 'Collection#ujk6nDuqFueXNGndXj8Qp9hU|#Collection',
+    items: [
+      {
+        id: '5ee0c486308f590260d9a07f',
+        type: 'paypal-referral',
+        externalId: 'DKTs3U38hdhfEqwF1JKoT2',
+        enabled: true,
+        params: {
+          centrapayMerchantId: '5ffcaf432003060007b98343',
+          email: 'test@centrapay.com'
+        },
+        status: 'provisioning',
+        claimedByAccountId: 'Jaim1Cu1Q55uooxSens6yk',
+        claimedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        claimedAt: '2020-06-12T01:17:46.499Z',
+        inProgress: true,
+        createdAt: '2020-06-11T01:17:46.499Z',
+        createdBy: 'crn:BIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        updatedAt: '2020-06-12T01:17:46.499Z',
+        updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+      },
+      {
+        id: '4maj50b6fdhmv0v38yrgjzdh',
+        type: 'paypal-referral',
+        externalId: 'alL3YQEGeY5WLu5uSFbM0tb3',
+        enabled: true,
+        params: {
+          centrapayMerchantId: '7aqasjid5kbcp7wquopoe4dg',
+          email: 'test2@centrapay.com'
+        },
+        status: 'provisioning',
+        claimedByAccountId: 'Jaim1Cu1Q55uooxSens6yk',
+        claimedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        claimedAt: '2020-07-12T01:18:46.499Z',
+        inProgress: true,
+        createdAt: '2020-07-11T01:18:46.499Z',
+        createdBy: 'crn:BIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+        updatedAt: '2020-07-12T01:18:46.499Z',
+        updatedBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey'
+      }
+    ]
+  }
+};
