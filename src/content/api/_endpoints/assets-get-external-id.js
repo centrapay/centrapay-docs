33f16ae9e96df434974b912037039c84f2c3f595
Diego Pupato <pupato.diego@gmail.com>
2024-09-12 12:51:58 +1200

    feat: add getAssetByExternalId (#1059)

---
+++ src/content/api/_endpoints/assets-get-external-id.js
@@ -0,0 +1,32 @@
+export default {
+  method: 'GET',
+  path: '/api/assets/centrapay.token.main/external-id/TiYi6haDzdkasmdfhIGz',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+  response: {
+    id: '3Liye29ypHRGm4pvdLWuUt',
+    accountId: 'gyTCZPpSbGZTBAFRa5Kgqo',
+    category: 'token',
+    type: 'centrapay.token.main',
+    liveness: 'main',
+    description: 'Centrapay Token',
+    createdAt: '2024-04-23T22:11:57.158Z',
+    status: 'active',
+    currency: 'NZD',
+    balance: '1234',
+    initialBalance: '5678',
+    availableBalance: '4321',
+    settlement: true,
+    approvalId: 'mX5BUsewoTPExN1kWNjoqn',
+    expiresAt: '2024-05-07T22:11:56.924Z',
+    activeFrom: '2024-04-23T22:11:56.924Z',
+    bankAccountId: 'KMHxO6dF38nbbJJSCGgCWR',
+    name: 'token',
+    period: 'month',
+    periodStartedAt: '2024-05-07T22:11:56.924Z',
+    periodEndingAt: '2024-05-09T22:11:56.924Z'
+  }
+};
