3de8f6f7a5b7f292617319555a69c6d593a5b880
Raymond Yang <raymondyang@outlook.co.nz>
2024-10-15 16:41:56 +1300

    Add additional fields to Settlement (#1072)

--- src/content/api/_endpoints/settlements-list-by-merchantId.js
+++ src/content/api/_endpoints/settlements-list-by-merchantId.js
@@ -18,7 +18,9 @@ export default {
         settledAt: '2021-11-13T11:59:59.999Z',
         settledBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
         settlementAmount: '2500',
-        settlementBankAccountId: '67e0c486308f590260d9a139'
+        settlementBankAccountId: '67e0c486308f590260d9a139',
+        type: 'centrapay',
+        test: false
       },
       {
         id: '9ds2cs89028sh90260d9f91h',
@@ -28,7 +30,9 @@ export default {
         createdAt: '2021-11-13T01:17:46.499Z',
         createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
         settlementAmount: '2200',
-        settlementBankAccountId: '67e0c486308f590260d9a139'
+        settlementBankAccountId: '67e0c486308f590260d9a139',
+        type: 'centrapay',
+        test: false
       }
     ]
   }
