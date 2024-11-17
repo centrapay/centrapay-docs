38f94ef7cd6350e5d8853f3551023be82a455e6e
Tom Blake <46909693+tom-blake@users.noreply.github.com>
2024-11-18 12:45:31 +1300

    Use expressive code to render code blocks (#1089)
    * Convert transaction reporting guide to markdoc
    
    - Convert transaction reporting guide to markdoc
    - Add new Astro components for API links and informational notes to
      avoid affecting existing MDX pages.
    - Add markdoc config to repository
    - Remove obselete docker compose version to prevent warning locally
    
    Test plan:
    - Confirm transaction reporting guide has no visual changes
    - Confirm API link correctly navigates to the right URL
    
    * Use expressive code to render code blocks
    
    Use expressive code to render code blocks instead of maintaining our own
    custom component.
    
    Key notes:
    - [ADR here](https://www.notion.so/centrapay/Use-expressive-code-to-render-code-blocks-in-our-docs-site-141a9ab17e8080c894aafa382f95e2c4)
    - I've chosen to use the material-theme-palenight. [Full available theme list here](https://expressive-code.com/guides/themes/)
    - Code blocks no longer include custom method and path props.
    - MDX file changes only include moving the code block title inline with
      the markdown fence backticks.
    
    Test plan:
    - Confirm that code blocks render correctly for all pages and API
      endpoints when running the dev server and when the site is being built
      statically

---
+++ ec.config.mjs
@@ -0,0 +1,8 @@
+import { defineEcConfig } from 'astro-expressive-code';
+
+export default defineEcConfig({
+  themes: ['material-theme-palenight'],
+  styleOverrides: {
+    borderRadius: '0.5rem',
+  },
+});
