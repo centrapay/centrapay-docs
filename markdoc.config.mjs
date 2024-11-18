3d8b175d8b184b1309e071e65e82ffc33f5d79af
Tom Blake <tom.blake@centrapay.com>
2024-11-18 20:16:32 +1300

    Convert patron not present guide to markdoc
    Add new CodeFence component to allow markdoc to render code fences using a
    custom component.
    
    The CodePanel component used by the MDX integration will be removed once
    we've migrated all guides to markdoc.
    
    Test plan:
    - Confirm no visual regressions on the patron not present guide
    - Confirm code fences on MDX files are being rendered correctly

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -2,6 +2,14 @@ import { defineMarkdocConfig, component, nodes } from '@astrojs/markdoc/config';
 
 export default defineMarkdocConfig({
   nodes: {
+    fence: {
+      attributes: {
+        ...nodes.fence.attributes,
+        title: { type: String, render: 'title' },
+        mark: { type: Number, render: 'mark' }
+      },
+      render: component('./src/components/CodeFence.astro'),
+    },
     link: {
       ...nodes.link,
       render: component('./src/components/Link.astro'),
