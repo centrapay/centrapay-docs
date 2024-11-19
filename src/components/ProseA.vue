361290f1055f6938a35368babc4cb1cdf2a0fce1
Tom Blake <tom.blake@centrapay.com>
2024-11-19 15:36:46 +1300

    Simplify link component
    The badge and external link icon in our API links isn't providing
    enough value to integrators to justify maintaining the extra
    complexity in the component.
    
    Test plan:
    - Confirm links in Vue components, MDX and markdoc pages no longer
      include the API badges or external link icons
    - Confirm links visually look the same otherwise

--- src/components/ProseA.vue
+++ src/components/ProseA.vue
@@ -2,33 +2,19 @@
   <a
     :href="props.href"
     :target="isExternalLink ? '_blank' : '_self'"
-    class="group inline-flex justify-center items-center text-interactive-secondary hover:text-interactive-secondary-hover"
+    class="not-prose underline font-medium inline-flex justify-center items-center text-interactive-primary hover:text-interactive-secondary-hover"
   >
-    <span class="underline"><slot /></span>
-    <span v-if="isApiReferenceLink">&nbsp;</span>
-    <span
-      v-if="isApiReferenceLink"
-      class="rounded-sm bg-interactive-secondary group-hover:bg-interactive-secondary-hover h-3 w-6 inline-flex justify-center items-center"
-    >
-      <span class="text-content-inverse-primary text-[10px]">&nbsp;API&nbsp;</span>
-    </span>
-    <ExternalLink
-      v-else-if="isExternalLink"
-      class="icon-sm h-3 w-6"
-    />
+    <slot />
   </a>
 </template>
 
 <script setup>
-import ExternalLink from './icons/ExternalLink.vue';
 const props = defineProps({
   href: {
     type: String,
     default: ''
   },
 });
-
-const isApiReferenceLink = props.href.startsWith('/api');
 
 const isExternalLink = (!props.href.startsWith('/') && !props.href.startsWith('#'));
 </script>
