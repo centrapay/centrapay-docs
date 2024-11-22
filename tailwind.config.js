3f4bc43ed389df93ac5a20d3526944aded336d92
Megan Steenkamp <megan.steenkamp@centrapay.com>
2024-11-22 13:09:02 +1100

    [⏰ 10% time]: Remove code.css
    Remove code.css class and move styling into tailwind config.
    
    Test plan: Expect inline code `like this` to look unchanged.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 const colors = require('./src/assets/css/colors');
 
-const disabledCss = {
-  'code::before': false,
-  'code::after': false,
-  pre: false,
-  code: false,
-  'pre code': false,
-};
-
 const tailwindConfig = {
   content: [
     './src/**/*.{astro,vue, js, css}',
@@ -18,7 +10,25 @@ const tailwindConfig = {
     },
     extend: {
       typography: {
-        DEFAULT: { css: disabledCss },
+        DEFAULT: {
+          css: {
+            'code::before': false,
+            'code::after': false,
+            'pre': false,
+            'code': false,
+            'pre code': false,
+            '*:not(pre) > code': {
+              'border-radius': '0.375rem',
+              'background-color': '#F8F8F8',
+              'border-width': '1px',
+              'border-color': '#D1D5DB',
+              'padding': '1px 3px',
+              'text-color': '#111827',
+              'font-size': '0.875rem',
+              'line-height': '1.25rem',
+            }
+          }
+        },
       },
       colors: {
         ...colors.light,
