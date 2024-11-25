669de8a02b9f2704f04fc5d5dfd45402b723bcb6
Tom Blake <tom.blake@centrapay.com>
2024-11-26 11:15:51 +1300

    Migrate mermaid diagrams to markdoc
    Migrate all MDX files containing mermaid diagrams to markdoc. It was
    easier to migrate all MDX files than it was to support mermaid with both
    MDX and markdoc.
    
    Additional necessary changes include:
    - Create a custom image component for markdoc. This also meant moving
      the farmlands card image out of the public folder
    - Add custom markdoc tags for grid lists and farmlands solution cards to
      allow our bespoke Farmlands connection page to be migrated to markdoc.
    - Removing our custom mermaid plugin in favour of mermaid's out of the
      box configuration.
    - Updating the markdoc integration config to ignore identation to
      support better readability.
    
    Test plan:
    - Confirm no visual regressions in each migrated guide using the dev
      environment

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -12,11 +12,40 @@ export default defineMarkdocConfig({
         title: { type: String, render: 'title' },
         mark: { type: Number, render: 'mark' }
       },
-      render: component('./src/components/CodeFence.astro'),
+      render: component('./src/components/Fence.astro'),
+    },
+    image: {
+      ...nodes.image,
+      render: component('./src/components/Figure.astro'),
+      attributes: {
+        src: { type: String },
+        alt: { type: String },
+      },
     },
     link: {
       ...nodes.link,
       render: component('./src/components/Link.astro'),
+    },
+  },
+  tags: {
+    farmlandsSolutionCard: {
+      render: component('./src/components/SolutionCard.astro'),
+      attributes: {
+        description: { type: String },
+        imageSrc: { type: String },
+        linkText: { type: String },
+        linkUrl: { type: String },
+        title: { type: String },
+      },
+    },
+    gridList: {
+      render: component('./src/components/Grid.astro'),
+      attributes: {
+        class: { type: String },
+      },
+    },
+    mermaid: {
+      render: component('./src/components/Mermaid.astro'),
     },
   },
 });
