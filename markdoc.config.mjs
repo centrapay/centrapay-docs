bde67f45da7ede603355643a3cb7221da875f0d6
Tom Blake <46909693+tom-blake@users.noreply.github.com>
2024-12-03 12:03:00 +1300

    Add link validation for markdoc pages (#1118)
    Fail the build when links:
    - Do not have an href
    - Are not masked by a readable text
    - Are to internal pages but are not relative
    
    Test plan:
    - Confirm the build fails for the cases above in markdoc files

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -25,6 +25,37 @@ export default defineMarkdocConfig({
     link: {
       ...nodes.link,
       render: component('./src/components/Link.astro'),
+      validate(node) {
+        const { href } = node.attributes;
+        if (!href) {
+          return [
+            {
+              id: 'link-href',
+              level: 'error',
+              message: `Link href is required.`,
+            }
+          ];
+        }
+        if (node.children.length !== 1 && !href.startsWith('mailto:')) {
+          return [
+            {
+              id: 'link-href',
+              level: 'error',
+              message: `Link URLs should be masked with readable text. See ${href}`,
+            }
+          ];
+        }
+        if (href.startsWith('https://docs.centrapay.com')) {
+          return [
+            {
+              id: 'link-href',
+              level: 'error',
+              message: `Internal link hrefs should be relative links. See ${href}`,
+            }
+          ];
+        }
+        return [];
+      },
     },
   },
   tags: {
