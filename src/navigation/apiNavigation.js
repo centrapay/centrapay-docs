7f5b70bd9036b48e480b6ebb39a164d71f16b49f
Tom Blake <46909693+tom-blake@users.noreply.github.com>
2024-12-09 11:43:01 +1300

    Add missing API docs (#1138)
    Add API docs that were accidentally removed from the sidebar in
    https://github.com/centrapay/centrapay-docs/pull/1132/files#diff-4460da6ebd489d74cd52390cd1fa1d5f28708b14f0c15ca687bde6092f3c9e9e
    
    Test plan:
    - Confirm missing docs are present in the API reference sidebar

--- src/navigation/apiNavigation.js
+++ src/navigation/apiNavigation.js
@@ -2,15 +2,22 @@ import { getCollection } from '../utils/getCollection';
 import Navigation from '../navigation/Navigation';
 
 const nav = [
+  { title: 'API Reference', order: 6 },
   { title: 'Accounts', order: 7 },
   { title: 'Assets', order: 8 },
   { title: 'Bank Accounts', order: 9 },
+  { title: 'Batches', order: 10 },
   { title: 'Events', order: 11 },
   { title: 'Integrations', order: 12 },
+  { title: 'Invitations', order: 13 },
   { title: 'Loyalty Programs', order: 14 },
+  { title: 'Media Uploads', order: 15 },
   { title: 'Merchants', order: 16 },
   { title: 'Payment Requests', order: 17 },
+  { title: 'Profiles', order: 18 },
+  { title: 'Quotas', order: 19 },
   { title: 'Scanned Codes', order: 20 },
+  { title: 'Settlements', order: 21 },
 ];
 
 const collections = await getCollection('api');
