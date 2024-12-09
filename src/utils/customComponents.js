1899526af4f288070e9bbe628ec71e005a025050
Tom Blake <46909693+tom-blake@users.noreply.github.com>
2024-12-10 10:22:35 +1300

    Remove custom components (#1143)
    Remove custom components leftover after migration from MDX to markdoc.
    
    Test plan:
    - Confirm no visual regressions where custom components were previously
      being used

--- src/utils/customComponents.js
+++
@@ -1,7 +0,0 @@
-import ProseBlockquote from '../components/ProseBlockquote.vue';
-import ProseA from '../components/ProseA.vue';
-
-export default {
-  a: ProseA,
-  blockquote: ProseBlockquote,
-};
