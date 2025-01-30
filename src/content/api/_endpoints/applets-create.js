4a2e57a156203582f9b8dac6c967c185ca8f3769
Sebastian Wells <sebastian.wells@centrapay.com>
2025-01-31 11:55:57 +1300

    create applet api

---
+++ src/content/api/_endpoints/applets-create.js
@@ -0,0 +1,24 @@
+export default {
+  method: 'POST',
+  path: '/api/appletss',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      name: 'Centrapay Cafe Applet',
+      mediaUploadId: '8aoMfscvtuewsuJzmzBzAs',
+      accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    },
+  },
+  response: {
+    id: 'WRhAxxWpTKb5U7pXyxQjjY',
+    name: 'Centrapay Cafe',
+    mediaUploadId: '8aoMfscvtuewsuJzmzBzAs',
+    img: 'https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    createdAt: '2021-08-25T00:02:49.488Z',
+    createdBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+  }
+};
