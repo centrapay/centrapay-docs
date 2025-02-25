10494795027f448f34de2e54f451861d8580eb0d
Braden Palmer <69737582+bpal938@users.noreply.github.com>
2025-02-25 16:47:40 +1300

    add docs for list applets api (#1172)
    * add docs for list applets api
    * add extension info to applet docs

--- src/content/api/_endpoints/applets-create.js
+++ src/content/api/_endpoints/applets-create.js
@@ -1,6 +1,6 @@
 export default {
   method: 'POST',
-  path: '/api/appletss',
+  path: '/api/applets',
   request: {
     headers: {
       'X-Api-Key': '<TOKEN>',
@@ -10,6 +10,15 @@ export default {
       name: 'Centrapay Cafe Applet',
       mediaUploadId: '8aoMfscvtuewsuJzmzBzAs',
       accountId: 'Jaim1Cu1Q55uooxSens6yk',
+      extensions: [
+        {
+          type: 'loyalty',
+          loyaltyProgramId: 'L001',
+          getPromotionsUrl: 'service.payap.com/api/loyaltyPrograms/L001/promotions',
+          getPromotionMembershipsUrl: 'service.payap.com/api/loyaltyPrograms/L001/promotionMemberships',
+          getLoyaltyProgramUrl: 'service.payap.com/api/loyaltyPrograms/L001'
+        }
+      ]
     },
   },
   response: {
