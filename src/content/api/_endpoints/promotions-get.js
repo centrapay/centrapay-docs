815298da1d3462cf4baf5cd3329493278fd3b408
Braden Palmer <69737582+bpal938@users.noreply.github.com>
2025-04-10 14:01:17 +1200

    add docs for get promotion api (#1195)

---
+++ src/content/api/_endpoints/promotions-get.js
@@ -0,0 +1,46 @@
+export default {
+  method: 'GET',
+  path: '/api/promotions/8aoMfscvtuewsuJzmzBzAs',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+  response: {
+    id: '8aoMfscvtuewsuJzmzBzAs',
+    accountId: '57SyRRgZ1U8Kq2wKpCnSR5',
+    programId: 'WRhAxxWpTKb5U7pXyxQjjY',
+    name: 'Spend a Buck',
+    summary: 'Make a payment',
+    description: 'The amount of the payment must be greater than $1.',
+    mediaUploadId: '26yUWG6wFgmva8UaDiCTWq',
+    img: 'https://media-upload.centrapay.com/image.png?jhbdsfau67ewejshb=487hsdjhbdgs743',
+    startsAt: '2023-02-16T00:47:54.131Z',
+    endsAt: '2024-02-16T00:47:54.131Z',
+    rewards: [
+      {
+        assetType: 'centrapay.nzd.main',
+        amount: '500',
+      }
+    ],
+    eventType: 'payment',
+    target: {
+      type: 'count',
+      amount: 1
+    },
+    type: 'challenge',
+    conditions: [
+      {
+        field: 'amount',
+        value: '100',
+        operator: 'greater-than'
+      }
+    ],
+    supplyLimit: '10',
+    remainingSupply: '7',
+    createdAt: '2023-02-08T04:04:27.426Z',
+    createdBy: 'crn::user:1234',
+    updatedAt: '2023-02-08T04:04:27.426Z',
+    updatedBy: 'crn::user:1234',
+  }
+};
