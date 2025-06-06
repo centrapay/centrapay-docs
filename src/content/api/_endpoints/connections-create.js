ca290825f2175e97ed9b37eb8995afa9b2cf0ff1
Ellen Sennikoff <45328108+fedch@users.noreply.github.com>
2025-06-06 15:17:18 +1200

    Write documentation for the new Connections API (#1213)
    * Write documentation for the new Connections API
    
    * Change double to single quotes
    
    * Add url property

---
+++ src/content/api/_endpoints/connections-create.js
@@ -0,0 +1,22 @@
+export default {
+  method: 'POST',
+  path: '/api/connections',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      merchantId: 'abcde',
+    },
+  },
+  response: {
+    id: '1234',
+    createdBy: 'crn:1234abc:api-key:MyAPIKey',
+    createdByAccountName: 'Salespoint',
+    createdAt: '2021-08-25T00:02:49.488Z',
+    status: 'created',
+    url: 'https://app.centrapay.com/connect/1234',
+    merchantId: 'abcde'
+  }
+};
