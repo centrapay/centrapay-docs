aad4f4a9506ad120d4045069d79f179e93b941b3
Ellen <fedchenko.yelena@gmail.com>
2025-06-06 15:07:00 +1200

    Add url property

--- src/content/api/_endpoints/connections-create.js
+++ src/content/api/_endpoints/connections-create.js
@@ -16,6 +16,7 @@ export default {
     createdByAccountName: 'Salespoint',
     createdAt: '2021-08-25T00:02:49.488Z',
     status: 'created',
+    url: 'https://app.centrapay.com/connect/1234',
     merchantId: 'abcde'
   }
 };
