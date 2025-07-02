42c101958a62955ab41f49b65e9a97afe2a1c05a
Ellen Sennikoff <45328108+fedch@users.noreply.github.com>
2025-07-02 14:51:15 +1200

    Add the link to authorize connection docs in the navigation panel (#1227)
    * Add the link to authorize connection docs in the navigation panel
    
    * Fix test
    
    * Add wallet name and change the id definition

---
+++ src/content/api/_endpoints/connections-authorize.js
@@ -0,0 +1,25 @@
+export default {
+  method: 'POST',
+  path: '/api/connections/1234/authorize',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      walletId: 'vendswift-nz',
+    },
+  },
+  response: {
+    id: '1234',
+    createdBy: 'crn:1234abc:api-key:MyAPIKey',
+    createdByAccountName: 'Salespoint',
+    createdAt: '2021-08-25T00:02:49.488Z',
+    status: 'created',
+    url: 'https://app.centrapay.com/connect/1234',
+    updatedBy: 'crn:1234abc:api-key:MyAPIKey',
+    updatedAt: '2022-08-25T00:02:49.488Z',
+    walletId: 'vendswift-nz',
+    walletName: 'VendSwift'
+  }
+};
