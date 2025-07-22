34f52d14845d8cab6e7216a1397b86edde314760
Braden Palmer <braden.palmer@centrapay.com>
2025-07-22 17:03:32 +1200

    add docs for get connection summary API

---
+++ src/content/api/_endpoints/connections-get-summary.js
@@ -0,0 +1,11 @@
+export default {
+  method: 'GET',
+  path: '/api/connections/1234/summary',
+  response: {
+    id: '1234',
+    status: 'created',
+    url: 'https://app.centrapay.com/connect/1234',
+    merchantId: 'abcde',
+    createdByAccountName: 'Salespoint',
+  }
+};
