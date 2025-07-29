be207d963ce4adc3ea72c37290ed005f6643181d
Raymond Yang <raymond.yang@centrapay.com>
2025-07-30 11:05:36 +1200

    Document connection statuses more clearly

--- src/content/api/_endpoints/connections-authorize.js
+++ src/content/api/_endpoints/connections-authorize.js
@@ -15,7 +15,7 @@ export default {
     createdBy: 'crn:1234abc:api-key:MyAPIKey',
     createdByAccountName: 'Salespoint',
     createdAt: '2021-08-25T00:02:49.488Z',
-    status: 'created',
+    status: 'authorized',
     url: 'https://app.centrapay.com/connect/1234',
     updatedBy: 'crn:1234abc:api-key:MyAPIKey',
     updatedAt: '2022-08-25T00:02:49.488Z',
