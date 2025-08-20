5f9e4f2658bd000cc54b99e66394decf4b3fed98
Raymond Yang <raymond.yang@centrapay.com>
2025-08-21 10:35:07 +1200

    Add assetCollectionId to asset xfer model

--- src/content/api/_endpoints/asset-transfers-get-summary.js
+++ src/content/api/_endpoints/asset-transfers-get-summary.js
@@ -8,6 +8,7 @@ export default {
     message: 'Happy birthday',
     senderName: 'My Cafe',
     createdAt: '2020-05-01T12:30:00.000Z',
-    url: 'https://app.centrapay.com/transfer/M7Kn2stAxNa6ri7h'
+    url: 'https://app.centrapay.com/transfer/M7Kn2stAxNa6ri7h',
+    assetCollectionId: 'c_Xv990BzkgfoDS7bBls50pd',
   }
 };
