e5c22ab52473104936eca5cdbf2ebd5528399740
alexBinns <alex.binns@centrapay.com>
2025-08-27 15:40:09 +1200

    Remove submenu for guides

--- src/navigation/guideNavigation.js
+++ src/navigation/guideNavigation.js
@@ -1,5 +1,4 @@
 import { getCollection } from '../utils/getCollection';
-import { render } from 'astro:content';
 import Navigation from '../navigation/Navigation';
 
 const nav = [
@@ -11,16 +10,11 @@ const nav = [
   { title: 'Developers' },
 ];
 
-const collections = await Promise.all([
+const content = await Promise.all([
   ...await getCollection('guides'),
   ...await getCollection('connections'),
 ]);
 
-const content = await Promise.all(collections.map(async page => {
-  const { headings } = await render(page);
-  page.headings = headings.filter(heading => heading.depth === 2);
-  return page;
-}));
 
 const navigation = Navigation.create({ nav, content });
 
