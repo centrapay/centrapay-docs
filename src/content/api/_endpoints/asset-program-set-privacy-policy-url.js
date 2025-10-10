22d81aaa7f83fd5b390561fce14c13e78e691da8
Caleb Wenborn <146693554+cwenborn@users.noreply.github.com>
2025-10-10 14:55:34 +1300

    Add set privacy policy url (#1260)
    - Add set terms of service url
    
    
    
    Fix typo
    
    Co-authored-by: Isaac Murtagh <39183091+IsaacMurtagh@users.noreply.github.com>

---
+++ src/content/api/_endpoints/asset-program-set-privacy-policy-url.js
@@ -0,0 +1,29 @@
+export default {
+  method: 'POST',
+  path: '/api/asset-programs/ap_WRhAxxWpTKb5U7pXyxQjjY/set-privacy-policy-url',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      privacyPolicyUrl: 'https://example.com/privacy-new',
+    },
+  },
+  response: {
+    id: 'ap_WRhAxxWpTKb5U7pXyxQjjY',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    assetTypes: [ 'payap-debit' ],
+    createdAt: '2021-08-25T00:02:49.488Z',
+    createdBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+    mediaUploadId: '8aoMfscvtuewsuJzmzBzAs',
+    name: 'My Asset Program',
+    privacyPolicyUrl: 'https://example.com/privacy-new',
+    published: true,
+    termsOfServiceUrl: 'https://example.com/terms',
+    test: true,
+    updatedAt: '2021-08-25T00:02:49.488Z',
+    updatedBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+    collectionIds: [ 'c_Xv990BzkgfoDS7bBls50pd' ],
+  }
+};
