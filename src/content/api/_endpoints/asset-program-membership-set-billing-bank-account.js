a8d9e2b71700c0c38fc00e1b1f61f527b0747cc9
Caleb Wenborn <caleb.wenborn@gmail.com>
2025-10-14 14:41:34 +1300

    add set asset program membership billing bank account endpoint

---
+++ src/content/api/_endpoints/asset-program-membership-set-billing-bank-account.js
@@ -0,0 +1,26 @@
+export default {
+  method: 'POST',
+  path: '/api/asset-program-memberships/apm_Xv990BzkgfoDS7bBls50pd/set-billing-bank-account',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+    payload: {
+      billingBankAccountId: 'WRhAxxWpTKb5U7pXyxQjjY'
+    },
+  },
+  response: {
+    id: 'apm_Xv990BzkgfoDS7bBls50pd',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    assetProgramId: 'ap_WRhAxxWpTKb5U7pXyxQjjY',
+    status: 'approved',
+    test: true,
+    createdAt: '2021-08-25T00:02:49.488Z',
+    createdBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+    updatedAt: '2021-08-25T00:02:49.488Z',
+    updatedBy: 'crn::user:b657195e-dc2f-11ea-8566-e7710d592c99',
+    tosAcceptedAt: '2021-08-25T00:03:15.123Z',
+    approvedAt: '2021-08-25T00:05:30.456Z',
+    billingBankAccountId: 'WRhAxxWpTKb5U7pXyxQjjY'
+  }
+};
