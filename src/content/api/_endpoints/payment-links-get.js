48177282bdea5fca023dd3b1f637e1980c81de5d
Han <contacthanahmae@gmail.com>
2025-12-02 16:08:14 +1300

    Feat/add payment link model and endpoints (#1287)
    * add payment link model and endpoint docs
    
    * Add payment link to create payment request endpoint

---
+++ src/content/api/_endpoints/payment-links-get.js
@@ -0,0 +1,21 @@
+export default {
+  method: 'GET',
+  path: '/api/payment-links/1234',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    }
+  },
+  response: {
+    id: '1234',
+    status: 'created',
+    name: 'Front Checkout',
+    merchantId: 'b040b262ddb80ea8',
+    paymentRequestId: '12d22ece51eb033',
+    createdBy: 'crn:1234abc:api-key:MyAPIKey',
+    createdAt: '2021-08-25T00:02:49.488Z',
+    updatedBy: 'crn:1234abc:api-key:MyAPIKey',
+    updatedAt: '2022-08-25T00:02:49.488Z',
+  }
+};
