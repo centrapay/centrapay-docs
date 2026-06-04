da74385d9d6b4e2041de78fa665befcd7b00d2d4
Linlin-Liang <linlin.liang@centrapay.com>
2026-06-04 15:00:53 +1200

    Add get patron code by namespace and external id to patron code page (#1347)

---
+++ src/content/api/_endpoints/patron-codes-get-by-externalId.js
@@ -0,0 +1,18 @@
+export default {
+  method: 'GET',
+  path: '/api/patron-codes/namespace-test/external-id/3e9003bbff48d',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+    },
+  },
+  response: {
+    id: 'V17FByEP9gm1shSG6a1Zzx',
+    accountId: 'Jaim1Cu1Q55uooxSens6yk',
+    barcode: '9990001234567895',
+    createdAt: '2021-06-08T22:55:00.000Z',
+    expiresAt: '2021-06-08T23:00:00.000Z',
+    externalId: '3e9003bbff48d',
+    appName: 'centrapay'
+  }
+};
