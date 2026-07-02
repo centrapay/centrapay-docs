0ae8c64ecec58836242087318b88d972e6f8132c
Chad Miller <52280296+chad-miller@users.noreply.github.com>
2026-07-02 12:02:23 +1200

    add merchant metadata to guides (#1361)

---
+++ src/content/api/_endpoints/merchants-set-metadata.js
@@ -0,0 +1,23 @@
+export default {
+  method: 'POST',
+  path: '/api/merchants/5ee0c486308f590260d9a07f/metadata',
+  request: {
+    headers: {
+      'X-Api-Key': '<TOKEN>',
+      'Content-Type': 'application/json',
+    },
+    payload: {
+      metadata: {
+        deviceId: 'device-001',
+      },
+    },
+  },
+  response: {
+    merchantId: 'MhocUmpxxmgdHjr7DgKoKw',
+    createdAt: '2021-09-12T01:11:22.491Z',
+    createdBy: 'crn:WIj211vFs9cNACwBb04vQw:api-key:MyApiKey',
+    metadata: {
+      deviceId: 'device-001',
+    },
+  },
+};
